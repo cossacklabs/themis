9249be2bd8f3848c97b6fea0d7d86df7276bd390
andrey <andrey@cossacklabs.com>
2014-12-16 22:54:11 +0200

    modify secure message wrap

--- src/themis/secure_message.c
+++ src/themis/secure_message.c
@@ -4,28 +4,42 @@
  * (c) CossackLabs
  */
 
-#include "themis/secure_message.h"
+#include <themis/secure_message.h>
+#include <themis/secure_message_wrapper.h>
 
-themis_secure_message_t* themis_secure_message_init(const uint8_t* private_key,
-						    const size_t private_key_length,
-						    const uint8_t* public_key,
-						    const size_t public_key_length){
+themis_status_t themis_secure_message_wrap(const uint8_t* private_key,
+					   const size_t private_key_length,
+					   const uint8_t* public_key,
+					   const size_t public_key_length,
+					   const uint8_t* message,
+					   const size_t message_length,
+					   uint8_t* wrapped_message,
+					   size_t* wrapped_message_length){
+  HERMES_CHECK(private_key!=NULL && private_key_length!=0 && message!=NULL && message_length!=0 && wrapped_message_length!=NULL);
+  themis_secure_message_signer_t* ctx=NULL;
+  if(public_key==NULL && public_key_length==0){ 
+    ctx = themis_secure_message_signer_init(private_key, private_key_length);
+    HERMES_CHECK(ctx!=NULL);
+    themis_status_t res=themis_secure_message_singer_proceed(ctx, message, message_length, wrapped_message, wrapped_message_length);
+    if(res!=HERMES_SUCCESS){
+      return res;
+    }
+    HERMES_CHECK(secure_message_singer_destroy(ctx)==HERMES_SUCCESS);
+    return HERMES_SUCCESS;
+  }else{
+    return HERMES_INVALID_PARAMETER;
+  }
+  return HERMES_SUCCESS;
+}
+
+themis_status_t themis_secure_message_unwrap(const uint8_t* private_key,
+					   const size_t private_key_length,
+					   const uint8_t* public_key,
+					   const size_t public_key_length,
+					   const uint8_t* wrapped_message,
+					   const size_t wrapped_message_length,
+					   uint8_t* message,
+					   size_t* message_length){
 
 
 }
-
-themis_status_t themis_secure_message_wrap(themis_secure_message_t* ctx,
-					   const uint8_t* message,
-					   const size_t message_length);
-
-themis_status_t themis_secure_message_unwrap(themis_secure_message_t* ctx,
-					     uint8_t* message,
-					     const size_t message_length);
-
-themis_status_t themis_secure_message_export(themis_secure_message_t* ctx,
-					     uint8_t* message,
-					     size_t* message_length);
-
-themis_status_t themis_secure_message_destroy(themis_secure_message_t* ctx);
-
-
--- src/themis/secure_message.h
+++ src/themis/secure_message.h
@@ -7,43 +7,27 @@
 #ifndef _THEMIS_SECURE_MESSAGE_H_
 #define _THEMIS_SECURE_MESSAGE_H_
 
-#include "themis/message.h"
+#include <themis/themis.h>
 
-struct themis_secure_message_type{
-  themis_message_t* message;
-  themis_message_t* signature;
-};
-
-typedef struct themis_secure_message_type themis_secure_message_t;
-
-
-
-themis_secure_message_t* themis_secure_message_init(const uint8_t* private_key,
-						    const size_t private_key_length,
-						    const uint8_t* public_key,
-						    const size_t public_key_length);
-
-themis_status_t themis_secure_message_set(themis_secure_message_t* ctx,
+themis_status_t themis_secure_message_wrap(const uint8_t* private_key,
+					   const size_t private_key_length,
+					   const uint8_t* public_key,
+					   const size_t public_key_length,
 					   const uint8_t* message,
-					   const size_t message_length);
+					   const size_t message_length,
+					   uint8_t* wrapped_message,
+					   size_t* wrapped_message_length);
 
-themis_status_t themis_secure_message_set_plain(themis_secure_message_t* ctx,
-						const uint8_t* message,
-						const size_t message_length);
+themis_status_t themis_secure_message_wrap(const uint8_t* private_key,
+					   const size_t private_key_length,
+					   const uint8_t* public_key,
+					   const size_t public_key_length,
+					   const uint8_t* wrapped_message,
+					   const size_t wrapped_message_length,
+					   uint8_t* message,
+					   size_t* message_length);
 
-themis_status_t themis_secure_message_get(themis_secure_message_t* ctx,
-					     uint8_t* message,
-					     size_t* message_length);
-
-themis_status_t themis_secure_message_get_clean(themis_secure_message_t* ctx,
-						uint8_t* message,
-						size_t* message_length);
-
-themis_status_t themis_secure_message_destroy(themis_secure_message_t* ctx);
-
-
-
-#endif /* _MESSAGE_H_ */
+#endif /* _THEMIS_SECURE_MESSAGE_H_ */
 
 
 
