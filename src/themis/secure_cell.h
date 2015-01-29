21affb40fb17215ee9756aafc165981370e9e4f1
andrey <andrey@cossacklabs.com>
2015-01-29 18:35:45 +0200

    Modifiув THEMIS secure_cell interface.

--- src/themis/secure_cell.h
+++ src/themis/secure_cell.h
@@ -7,39 +7,63 @@
 #ifndef _SECURE_CELL_H_
 #define _SECURE_CELL_H_
 
-typedef struct themis_file_type themis_file_t;
+typedef struct themis_secure_cell_type themis_secure_cell_t;
 
-struct themis_file_interface_type{
-  FILE* file;
-  char* filename;
-  uint8_t mode;
-  FILE (*fopen)( const char * filename, const char * mode );
-  size_t (*fread)( void *ptr, size_t size_of_elements, size_t number_of_elements, FILE *a_file);
-  size_t (*fwrite)( const void *ptr, size_t size_of_elements, size_t number_of_elements, FILE *a_file);
-  int (*fclose)( FILE *fp );
-};
+themis_secure_cell_t* themis_secure_cell_create(const uint8_t* master_key, const size_t master_key_length);
 
-typedef struct themis_file_interface_type themis_file_interface_t;
+themis_status_t themis_secure_cell_encrypt_full(themis_secure_cell_t* ctx,
+					       const uint8_t* message,
+					       const size_t message_length,
+					       uint8_t* encrypted_message,
+					       size_t* encrypted_message_length);
 
-struct themis_keydb_interface_type{
-  themis_file_interface_t* db;
-  themis_status_t (* get_key)(themis_file_interface_t* db, const uint8_t* id, const size id_length, uint8_t* key, size_t* key_size);
- 
-};
+themis_status_t themis_secure_cell_decrypt_full(themis_secure_cell_t* ctx,
+					       const uint8_t* encrypted_message,
+					       const size_t encrypted_message_length,
+					       uint8_t* plain_message,
+					       size_t* plain_message_length);
 
+themis_status_t themis_secure_cell_encrypt_auto_split(themis_secure_cell_t* ctx,
+					       const uint8_t* message,
+					       const size_t message_length,
+					       uint8_t* iv,
+					       size_t* iv_length,
+					       uint8_t* auth_tag,
+					       size_t* auth_tag_length,
+					       uint8_t* encrypted_message,
+					       size_t* encrypted_message_length);
 
-typedef struct themis_keydb_interface_type themis_keydb_interface_t;
+themis_status_t themis_secure_cell_decrypt_auto_split(themis_secure_cell_t* ctx,
+					       const uint8_t* encrypted_message,
+					       const size_t encrypted_message_length,
+					       const uint8_t* iv,
+					       const size_t iv_length,
+					       const uint8_t* auth_tag,
+					       const size_t auth_tag_length,
+					       uint8_t* plain_message,
+					       size_t* plain_message_length);
 
-themis_file_t* themis_file_open(const uint8_t* master_key,
-				const size_t master_key_length,
-				themis_file_interface_t* file,
-				themis_keydb_interface_t* key_db);
+themis_status_t themis_secure_cell_encrypt_user_split(themis_secure_cell_t* ctx,
+					       const uint8_t* message,
+					       const size_t message_length,
+					       const uint8_t* iv,
+					       const size_t iv_length,
+					       uint8_t* auth_tag,
+					       size_t* auth_tag_length,
+					       uint8_t* encrypted_message,
+					       size_t* encrypted_message_length);
 
-themis_status_t themis_file_read(themis_file_t* file, uint8_t* buffer, size_t* buffer_length);
+themis_status_t themis_secure_cell_decrypt_user_split(themis_secure_cell_t* ctx,
+					       const uint8_t* encrypted_message,
+					       const size_t encrypted_message_length,
+					       const uint8_t* iv,
+					       const size_t iv_length,
+					       const uint8_t* auth_tag,
+					       const size_t auth_tag_length,
+					       uint8_t* plain_message,
+					       size_t* plain_message_length);
 
-themis_ststus_t themis_file_write(themis_file_t* file, const uint8_t* buffer, const size_t buffer_length);
-
-themis_status_t themis_file_close(themis_file_t* file);
+themis_status_t themis_secure_cell_destroy(themis_secure_cell_t* ctx);
 
 
 #endif /* _SECURE_CELL_H_ */
--- src/themis/themis.h
+++ src/themis/themis.h
@@ -7,6 +7,10 @@
 #ifndef THEMIS_H
 #define THEMIS_H
 
+#include <stdint.h>
+#include <stddef.h>
+typedef int themis_status_t;
+
 #include <themis/secure_message.h>
 
 #endif /* THEMIS_H */
