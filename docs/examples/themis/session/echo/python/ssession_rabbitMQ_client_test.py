d00bdd8024a2f0b7b26e02441be9235bdfc3c522
andrey <andrey@cossacklabs.com>
2015-03-07 19:50:13 +0200

    Add rabbitMQ python semples for secure session.

---
+++ docs/examples/themis/session/echo/python/ssession_rabbitMQ_client_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import ssession;
+import ssession_mem_transport;
+import pika
+import uuid
+
+client_priv = str('\x52\x45\x43\x32\x00\x00\x00\x2d\x51\xf4\xaa\x72\x00\x9f\x0f\x09\xce\xbe\x09\x33\xc2\x5e\x9a\x05\x99\x53\x9d\xb2\x32\xa2\x34\x64\x7a\xde\xde\x83\x8f\x65\xa9\x2a\x14\x6d\xaa\x90\x01');
+
+server_pub  = str('\x55\x45\x43\x32\x00\x00\x00\x2d\x75\x58\x33\xd4\x02\x12\xdf\x1f\xe9\xea\x48\x11\xe1\xf9\x71\x8e\x24\x11\xcb\xfd\xc0\xa3\x6e\xd6\xac\x88\xb6\x44\xc2\x9a\x24\x84\xee\x50\x4c\x3e\xa0');
+
+class transport(ssession_mem_transport.mem_transport):
+    def get_pub_key_by_id(self, user_id):
+        if user_id != "server":
+            raise Exception("no such id");
+        return server_pub;
+
+session=ssession.ssession("client", client_priv, transport());
+
+class SsessionRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.status, self.response = session.unwrap(body)
+
+    def call(self, message):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='ssession_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=message)
+        while self.response is None:
+            self.connection.process_data_events()
+        return (self.status, self.response)
+
+ssession_rpc = SsessionRpcClient()
+
+res, response = ssession_rpc.call(session.connect_request());
+while res==1:
+    res, response = ssession_rpc.call(response);
+
+res, response = ssession_rpc.call(session.wrap("This is test message!"));
+res, response = ssession_rpc.call(session.wrap("This is test message #2!"));
+res, response = ssession_rpc.call(session.wrap("This is test message #3!"));
+res, response = ssession_rpc.call(session.wrap("This is test message #4!"));
+
+
+print response;
