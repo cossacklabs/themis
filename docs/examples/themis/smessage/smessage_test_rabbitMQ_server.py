1a07ec3028eb220dc7da830e3dd425ec2dbe8eba
andrey <andrey@cossacklabs.com>
2015-03-10 20:55:21 +0200

    Add secure message twisted, tornado and rabbitMQ examples

---
+++ docs/examples/themis/smessage/smessage_test_rabbitMQ_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import smessage;
+import pika;
+
+client_pub = str('\x55\x45\x43\x32\x00\x00\x00\x2d\x13\x8b\xdf\x0c\x02\x1f\x09\x88\x39\xd9\x73\x3a\x84\x8f\xa8\x50\xd9\x2b\xed\x3d\x38\xcf\x1d\xd0\xce\xf4\xae\xdb\xcf\xaf\xcb\x6b\xa5\x4a\x08\x11\x21');
+
+server_priv= str('\x52\x45\x43\x32\x00\x00\x00\x2d\x49\x87\x04\x6b\x00\xf2\x06\x07\x7d\xc7\x1c\x59\xa1\x8f\x39\xfc\x94\x81\x3f\x9e\xc5\xba\x70\x6f\x93\x08\x8d\xe3\x85\x82\x5b\xf8\x3f\xc6\x9f\x0b\xdf');
+
+encrypter=smessage.themis_smessage_encrypter(server_priv, client_pub);
+decrypter=smessage.themis_smessage_decrypter(server_priv, client_pub);
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='smessage_queue')
+
+def on_request(ch, method, props, body):
+    message = decrypter.decrypt(body);
+    print message;
+    ch.basic_publish(exchange='', routing_key=props.reply_to, properties=pika.BasicProperties(correlation_id = props.correlation_id), body=encrypter.encrypt(message));
+    ch.basic_ack(delivery_tag = method.delivery_tag);
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='smessage_queue')
+
+channel.start_consuming()
---
+++ docs/examples/themis/smessage/smessage_test_tornado_client.py
@@ -0,0 +1,21 @@
+import smessage;
+import tornado.ioloop
+import tornado.httpclient;
+
+client_priv = str('\x52\x45\x43\x32\x00\x00\x00\x2d\x51\xf4\xaa\x72\x00\x9f\x0f\x09\xce\xbe\x09\x33\xc2\x5e\x9a\x05\x99\x53\x9d\xb2\x32\xa2\x34\x64\x7a\xde\xde\x83\x8f\x65\xa9\x2a\x14\x6d\xaa\x90\x01');
+
+server_pub  = str('\x55\x45\x43\x32\x00\x00\x00\x2d\x75\x58\x33\xd4\x02\x12\xdf\x1f\xe9\xea\x48\x11\xe1\xf9\x71\x8e\x24\x11\xcb\xfd\xc0\xa3\x6e\xd6\xac\x88\xb6\x44\xc2\x9a\x24\x84\xee\x50\x4c\x3e\xa0');
+
+http_client = tornado.httpclient.HTTPClient();
+encrypter=smessage.themis_smessage_encrypter(client_priv, server_pub);
+decrypter=smessage.themis_smessage_decrypter(client_priv, server_pub);
+try:
+    response = http_client.fetch(tornado.httpclient.HTTPRequest("http://127.0.0.1:26260", "POST", None, encrypter.encrypt("This is test message")));
+    message = decrypter.decrypt(response.body);
+    print message;
+    
+except tornado.httpclient.HTTPError as e:
+    print("Error: " + str(e));
+except Exception as e:
+    print("Error: " + str(e));
+http_client.close();
