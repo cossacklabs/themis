574c4970ea380f70ba23e7056e7a57e795346999
Ignat <ignat@cossacklabs.com>
2015-04-14 01:49:47 +0900

    Initial Android.mk for building native part for Android

---
+++ jni/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libsoter
+
+LOCAL_SRC_FILES := $(patsubst jni/%,%, $(wildcard $(LOCAL_PATH)/../src/soter/*.c))
+LOCAL_SRC_FILES += $(patsubst jni/%,%, $(wildcard $(LOCAL_PATH)/../src/soter/openssl/*.c))
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../src $(LOCAL_PATH)
+LOCAL_CFLAGS := -DOPENSSL
+
+LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := soter_test
+
+LOCAL_SRC_FILES := $(patsubst jni/%,%, $(wildcard $(LOCAL_PATH)/../tests/soter/*.c))
+LOCAL_SRC_FILES += $(patsubst jni/%,%, $(wildcard $(LOCAL_PATH)/../tests/common/*.c))
+
+LOCAL_SHARED_LIBRARIES := libsoter
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../src $(LOCAL_PATH)/../tests
+
+LOCAL_LDFLAGS := -Wl,--allow-shlib-undefined
+
+include $(BUILD_EXECUTABLE)
--- src/soter/openssl/soter.mk
+++ src/soter/openssl/soter.mk
@@ -1,8 +1,8 @@
 SOTER_SRC += $(wildcard $(CRYPTO_ENGINE)/*.c)
 
 # Put path to your OpenSSL/LibreSSL here
-OPENSSL_INCLUDE_DIR = libs/librebin/include
-OPENSSL_LIB_DIR = libs/librebin/lib/$(SDK)-$(ARCH)
+OPENSSL_DIR = libs/librebin
 
-CFLAGS += -I$(OPENSSL_INCLUDE_DIR)
-LDFLAGS += -L$(OPENSSL_LIB_DIR) -lcrypto
+CFLAGS += -I$(OPENSSL_DIR)/include
+LDFLAGS += -L$(OPENSSL_DIR)/lib -lcrypto
+
