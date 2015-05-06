1cba402b3a2063a33fde0072f698f5cd308eea6a
andrey <andrey@cossacklabs.com>
2015-05-06 15:08:37 +0300

    Add needed for pip files

--- docs/examples/python/smessage_test_server.py
+++ docs/examples/python/smessage_test_server.py
@@ -23,15 +23,16 @@ client_pub = str('\x55\x45\x43\x32\x00\x00\x00\x2d\x13\x8b\xdf\x0c\x02\x1f\x09\x
 
 server_priv= str('\x52\x45\x43\x32\x00\x00\x00\x2d\x49\x87\x04\x6b\x00\xf2\x06\x07\x7d\xc7\x1c\x59\xa1\x8f\x39\xfc\x94\x81\x3f\x9e\xc5\xba\x70\x6f\x93\x08\x8d\xe3\x85\x82\x5b\xf8\x3f\xc6\x9f\x0b\xdf');
 
-conn=smessage_ssocket.ssocket(priv_key=server_priv);
+conn=smessage_ssocket.ssocket(server_priv);
 conn.bind(("127.0.0.1", 26260));
 conn.listen(1);
 accepted, addr = conn.accept();
-
+print 1
 accepted.set_peer_pub_key(client_pub);
 
 while True:
     try:
+	print 2;
         message = accepted.recv(1024); #receive message
         print message;
         if message == "finish": #"finish" - last message
