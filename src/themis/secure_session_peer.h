1872ac5d612b619a4130008c135a7bf516bee574
andrey <andrey@cossacklabs.com>
2015-05-12 21:34:25 +0300

    Add license to java and change (c)

--- src/themis/secure_session_peer.h
+++ src/themis/secure_session_peer.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2015 CossackLabs
+* Copyright (c) 2015 Cossack Labs Limited
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
