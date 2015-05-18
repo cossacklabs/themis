f4198c3a28e6d20263817bf69d5400a44f4a0af9
vixentael <vixentael@gmail.com>
2015-05-19 02:54:11 +0300

    update iOS project to contain latest examples from docs; to be more dev-friendly

--- docs/examples/objc/ThemisTest/ThemisTest/Classes/ViewController.m
+++ docs/examples/objc/ThemisTest/ThemisTest/Classes/ViewController.m
@@ -22,14 +22,6 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+// Look inside console, young padavan!
 
 @end
