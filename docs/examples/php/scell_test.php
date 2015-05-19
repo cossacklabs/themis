c7c55309d7b09a6b46e8502f6defbc93b054571d
andrey <andrey@andrey-laptop>
2015-05-19 21:48:46 +0300

    Complete php example

--- docs/examples/php/scell_test.php
+++ docs/examples/php/scell_test.php
@@ -14,6 +14,22 @@
     echo $plain_message;
     echo '<br>';
 
+    $encrypted_message = phpthemis_scell_auto_split_encrypt($key, $message, $context);
+
+    echo bin2hex($encrypted_message['encrypted_message']);
+    echo bin2hex($encrypted_message['additional_auth_data']);
+    echo '<br>'; 
+    $plain_message2 = phpthemis_scell_auto_split_decrypt($key, $encrypted_message['encrypted_message'],  $encrypted_message['additional_auth_data'], $context);
+    echo $plain_message2;
+    echo '<br>';
+
+    $encrypted_message = phpthemis_scell_user_split_encrypt($key, $message, $context);
+
+    echo bin2hex($encrypted_message);
+    echo '<br>'; 
+    $plain_message3 = phpthemis_scell_user_split_decrypt($key, $encrypted_message, $context);
+    echo $plain_message3;
+
     echo '<br></body></html>';
 
 ?>
