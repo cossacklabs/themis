6974950431b38028ab58333b0293a70263f3e817
andrey <andrey@cossacklabs.com>
2015-07-23 11:56:26 +0300

    Add ios test to main test script

--- tests/check_ios_test.sh
+++ tests/check_ios_test.sh
@@ -2,8 +2,11 @@
 
 
 while true; do
-    BUILD_STATUS=`curl --header "Accept: application/json" https://circleci.com/api/v1/project/mnaza/objcthemis-tests/$1?circle-token==$OBJCTHEMIS_TEST_TOKEN | python -c 'import sys; import json; print(json.load(sys.stdin)["status"])'`
-
+    BUILD_STATUS=`curl --header "Accept: application/json" https://circleci.com/api/v1/project/mnaza/objcthemis-tests/$1 | python -c 'import sys; import json; print(json.load(sys.stdin)["status"])'`
+    if [[$? = 1]]
+	echo "Can't get build $1 status"
+	then exit 1
+    fi
     echo $BUILD_STATUS
     if [[ $BUILD_STATUS = "success" ]] 
 	then exit 0
