761294b3ab8ef04e9bb19b1376e4ad4c1f5e87eb
andrey <andrey@cossacklabs.com>
2015-07-27 10:34:26 +0300

    Add key werification to SCell and SMessage class

--- src/wrappers/themis/Obj-C/objcthemis/smessage.m
+++ src/wrappers/themis/Obj-C/objcthemis/smessage.m
@@ -40,6 +40,11 @@
         self.privateKey = [privateKey copy];
         self.publicKey = [peerPublicKey copy];
         self.mode = TSMessageModeEncryptDecrypt;
+	NSString* ms = @"123";
+	NSDtata m[] =  [ms dataUsingEncoding:NSUTF8StringEncoding];
+	NSError* error=NULL;
+	NSData* s=[self wrapData:m error:error];
+	if(!s){return nil;}
     }
     return self;
 }
@@ -51,6 +56,11 @@
         self.privateKey = [privateKey copy];
         self.publicKey = [peerPublicKey copy];
         self.mode = TSMessageModeSignVerify;
+	NSString* ms = @"123";
+	NSDtata m[] =  [ms dataUsingEncoding:NSUTF8StringEncoding];
+	NSError* error=NULL;
+	NSData* s=[self wrapData:m error:error];
+	if(!s){return nil;}
     }
     return self;
 }
