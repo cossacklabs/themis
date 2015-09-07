8398ce651ed0405f4f425028707a868e87da6246
mnaza <andrey@cossacklabs.com>
2015-09-07 17:26:03 +0300

    Correct ios tests

--- tests/check_ios_test.sh
+++ tests/check_ios_test.sh
@@ -2,7 +2,7 @@
 
 
 while true; do
-    BUILD_STATUS=`curl --header "Accept: application/json" https://circleci.com/api/v1/project/cossacklabs/themis-ios-tests/$1 | python -c 'import sys; import json; print(json.load(sys.stdin)["status"])'`
+    BUILD_STATUS=`curl --header "Accept: application/json" https://circleci.com/api/v1/project/cossacklabs/themis-ios-tests/$1?circle-token=$OBJCTHEMIS_TEST_TOKEN | python -c 'import sys; import json; print(json.load(sys.stdin)["status"])'`
     if [ "$?" != "0" ]
 	then echo "Can't get build $1 status"
 	exit 1
