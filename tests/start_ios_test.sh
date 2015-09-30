38629d94e4a2be386a3deba321ddf65893d0bb07
andrey <andrey@cossacklabs.com>
2015-09-30 20:00:27 +0300

    Correct some warnings

--- tests/start_ios_test.sh
+++ tests/start_ios_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-START_BUILD_NUMBER=`curl -X POST --header "Accept: application/json" https://circleci.com/api/v1/project/$REP_OWNER/themis-ios-tests/tree/master\?circle-token=$OBJCTHEMIS_TEST_TOKEN | python -m json.tool | sed -n -e '/"build_num":/ s/^.*"build_num": \(.*\),/\1/p' | sed -n -e '1s/^\([0-9]*\).*/\1/p'`
+START_BUILD_NUMBER=`curl -X POST --header "Accept: application/json" https://circleci.com/api/v1/project/cossacklabs/themis-ios-tests/tree/master\?circle-token=$OBJCTHEMIS_TEST_TOKEN | python -m json.tool | sed -n -e '/"build_num":/ s/^.*"build_num": \(.*\),/\1/p' | sed -n -e '1s/^\([0-9]*\).*/\1/p'`
 echo $START_BUILD_NUMBER
