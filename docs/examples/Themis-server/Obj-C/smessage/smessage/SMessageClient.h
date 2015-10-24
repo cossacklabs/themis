062856f82bcaf844b5820716f35ff65373c7a533
Admin <admin@Mac-Admin.local>
2015-10-24 13:17:14 +0300

    Add Themis CI client for smessage

--- docs/examples/Themis-server/Obj-C/smessage/smessage/SMessageClient.h
+++ docs/examples/Themis-server/Obj-C/smessage/smessage/SMessageClient.h
@@ -1,13 +1,8 @@
-//
-//  SMessageClient.h
-//  smessage
-//
-//  Created by Admin on 22.10.15.
-//  Copyright © 2015 Cossacklabs. All rights reserved.
-//
-
-#ifndef SMessageClient_h
-#define SMessageClient_h
+#import <Foundation/Foundation.h>
 
 
-#endif /* SMessageClient_h */
+@interface SMessageClient : NSObject
+
+- (void)secureMessageCITest;
+
+@end
