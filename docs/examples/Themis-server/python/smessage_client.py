c055f20586bd2ab38f6d881013f8d1c2168c9772
andrey <andrey@cossacklabs.com>
2015-10-28 21:49:22 +0200

    Add java themis server example

--- docs/examples/Themis-server/python/smessage_client.py
+++ docs/examples/Themis-server/python/smessage_client.py
@@ -5,13 +5,14 @@ from pythemis import smessage
 import base64
 import time;
 
-message_maker=smessage.smessage(base64.b64decode("UkVDMgAAAC3219ClAMr8iP+6YQw8owIaRPnZKQsV4Ch9OWbXJZ2jeeJQIpO6"), base64.b64decode("VUVDMgAAAC3OHpItAicSfMWzdsTs0W5saoV/sgCBWXP2qIsGStq9r2Y4b0Jr"))
-message = base64.b64encode(message_maker.wrap("Hello Themis CI server"))
+message_maker=smessage.smessage(base64.b64decode("UkVDMgAAAC1whm6SAJ7vIP18Kq5QXgLd413DMjnb6Z5jAeiRgUeekMqMC0+x"), base64.b64decode("VUVDMgAAAC1NY6NZAz62ZG25YU67a5zX1iDo0Aijz8xOu4nzE0yQhtJIQBbD"))
+message = base64.b64encode(message_maker.wrap("This is test message"))
 params = urllib.urlencode({'message': message})
 headers = {"Content-type": "application/x-www-form-urlencoded"}
 conn = httplib.HTTPSConnection("themis.cossacklabs.com")
-conn.request("POST", "/api/moxxyBpVqFouTPa/", params, headers)
+conn.request("POST", "/api/OiJLrqNZZvUcaCH/", params, headers)
 response_secure_message = conn.getresponse().read()
+print(len(response_secure_message))
 response_message=message_maker.unwrap(response_secure_message)
 print(response_message)
 conn.close()
--- docs/examples/Themis-server/python/ssession_client.py
+++ docs/examples/Themis-server/python/ssession_client.py
@@ -8,28 +8,30 @@ import time;
 
 class transport(ssession.mem_transport):
     def get_pub_key_by_id(self, user_id):        #necessary callback function
-        if user_id != b"bdKBEanUfQRgwmc":                        #we have only one peer with id "server"
+        if user_id != b"nXQpVsglEGFJgfK":                        #we have only one peer with id "server"
             raise Exception("no such id");
-        return base64.b64decode("VUVDMgAAAC0JPqWZA3Q39x/usK0YGphEx5PHclnJ1cY9X7v/OIV0l5pTmrpz"); 
+        return base64.b64decode("VUVDMgAAAC2yfQpBAzarlWrwxjlDduEH6Wdfah46bDVLGGCAqYYkVS+dlrcV"); 
 
-session=ssession.ssession(b"ckLNYmsOxvbxYGy",base64.b64decode("UkVDMgAAAC1whm6SAJ7vIP18Kq5QXgLd413DMjnb6Z5jAeiRgUeekMqMC0+x"), transport())
+session=ssession.ssession(b"gounDbllopcCcVB",base64.b64decode("UkVDMgAAAC1whm6SAJ7vIP18Kq5QXgLd413DMjnb6Z5jAeiRgUeekMqMC0+x"), transport())
 data = session.connect_request()
 params = urllib.urlencode({'message': base64.b64encode(data)})
 headers = {"Content-type": "application/x-www-form-urlencoded"}
 conn = httplib.HTTPSConnection("themis.cossacklabs.com")
-conn.request("POST", "/api/ckLNYmsOxvbxYGy/", params, headers)
+print params
+conn.request("POST", "/api/gounDbllopcCcVB/", params, headers)
 resp= conn.getresponse()
 print resp.status
 response_msg = session.unwrap(resp.read())
 while response_msg.is_control:
     params = urllib.urlencode({'message': base64.b64encode(response_msg)})
-    conn.request("POST", "/api/ckLNYmsOxvbxYGy/", params, headers)
+    print params
+    conn.request("POST", "/api/gounDbllopcCcVB/", params, headers)
     resp= conn.getresponse()
     print resp.status
     response_msg = session.unwrap(resp.read())
 
 params = urllib.urlencode({'message': base64.b64encode(session.wrap(b"Hello Themis CI server"))})
-conn.request("POST", "/api/ckLNYmsOxvbxYGy/", params, headers)
+conn.request("POST", "/api/gounDbllopcCcVB/", params, headers)
 response_msg = session.unwrap(conn.getresponse().read())
 print(response_msg)
 
