5a4a5ffc2e842e2794852dc0bd917fa565781da5
andrey <andrey@cossacklabs.com>
2016-01-04 09:12:32 +0200

    Add ThemisPP targets to Makefile

--- src/wrappers/themis/themispp/secure_session.hpp
+++ src/wrappers/themis/themispp/secure_session.hpp
@@ -45,8 +45,7 @@ namespace themispp{
 
     secure_session_t(const std::string& id, const data_t& priv_key, secure_session_callback_interface_t* callbacks):
       _session(NULL),
-      _res(0),
-      _res_flag(false){
+      _res(0){
       _callback.get_public_key_for_id=themispp::get_public_key_for_id_callback;
       _callback.user_data=callbacks;
       _session=secure_session_create(id.c_str(), id.length(), &priv_key[0], priv_key.size(), &_callback);
@@ -61,13 +60,12 @@ namespace themispp{
     const data_t& unwrap(const std::vector<uint8_t>& data){
       size_t unwrapped_data_length=0;
       themis_status_t r=secure_session_unwrap(_session, &data[0],data.size(), NULL, &unwrapped_data_length);
-      if(r==THEMIS_SUCCESS){_res_flag=false;_res.resize(0); return _res;}
+      if(r==THEMIS_SUCCESS){_res.resize(0); return _res;}
       if(r!=THEMIS_BUFFER_TOO_SMALL)
 	throw themispp::exception_t("secure_session_unwrap (length determination) failed");
       _res.resize(unwrapped_data_length);
       r=secure_session_unwrap(_session, &data[0],data.size(), &_res[0], &unwrapped_data_length);
-      if(r==THEMIS_SSESSION_SEND_OUTPUT_TO_PEER){_res_flag=true; return _res;}
-      _res_flag=false;
+      if(r==THEMIS_SSESSION_SEND_OUTPUT_TO_PEER){return _res;}
       if(r!=THEMIS_SUCCESS)
 	throw themispp::exception_t("secure_session_unwrap failed");
       return _res;
@@ -90,18 +88,15 @@ namespace themispp{
       _res.resize(init_data_length);
       if(secure_session_generate_connect_request(_session, &_res[0], &init_data_length)!=THEMIS_SUCCESS)
 	throw themispp::exception_t("secure_session_generate_connect_request failed");
-      _res_flag=true;
       return _res;
     }
     
-    const bool is_control(){return _res_flag;}
     const bool is_established(){return secure_session_is_established(_session);}
 
   private:
     ::secure_session_t* _session;
     ::secure_session_user_callbacks_t _callback;
     std::vector<uint8_t> _res;
-    bool _res_flag;
   };
 }// ns themis
 
--- tests/themispp/secure_session_test.hpp
+++ tests/themispp/secure_session_test.hpp
@@ -19,12 +19,7 @@
 
 #include <common/sput.h>
 #include <string.h>
-#include <queue>
-#include <algorithm>
-#include <boost/thread/thread.hpp>
 #include <themispp/secure_session.hpp>
-
-
 
 namespace themispp{
   namespace secure_session_test{
@@ -34,31 +29,6 @@ namespace themispp{
 
     const uint8_t client_priv[]={0x52, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0xf2, 0x6a, 0x06, 0x8c, 0x00, 0x5b, 0xd7, 0x35, 0x5c, 0x53, 0x41, 0x89, 0xcd, 0x08, 0xa2, 0x30, 0xd2, 0x0b, 0x32, 0x4c, 0x74, 0x87, 0xc9, 0x0c, 0x2e, 0x9c, 0x86, 0xbf, 0x40, 0xf6, 0xea, 0xac, 0x8a, 0x75, 0x63, 0x67, 0xbc};
     const uint8_t client_pub[]={0x55, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0xba, 0x94, 0x97, 0xf7, 0x03, 0x06, 0x4f, 0x4c, 0xe0, 0x82, 0x74, 0xe3, 0xe9, 0x23, 0xa3, 0xac, 0x57, 0x1f, 0x1e, 0xf8, 0xf0, 0x66, 0x85, 0xee, 0xa3, 0x4c, 0x4f, 0xd5, 0xb9, 0x85, 0xc2, 0xde, 0x39, 0x49, 0x8d, 0x99, 0xe1};
-
-    std::queue<std::vector<uint8_t> > qsc;
-    std::queue<std::vector<uint8_t> > qcs;
-
-    class transport{
-    public:
-      transport(std::queue<std::vector<uint8_t> >& to_read, std::queue<std::vector<uint8_t> >& to_write):
-	_tr(to_read),
-	_tw(to_write){}
-
-      const std::vector<uint8_t> read(){
-	while(_tr.empty()){sleep(1);}
-	std::vector<uint8_t> a=_tr.front();
-	_tr.pop();
-	return a;
-      }
-
-      void write(const std::vector<uint8_t>& data){
-	_tw.push(data);
-      }
-
-    private:
-      std::queue<std::vector<uint8_t> >& _tr;
-      std::queue<std::vector<uint8_t> >& _tw;
-    };
 
 
     class callback: public themispp::secure_session_callback_interface_t{
@@ -73,39 +43,44 @@ namespace themispp{
       }
     };
 
-
-    void client_func(){
-      std::string mes("the test message");
-      callback callbacks;
-      transport tr(qsc, qcs);
-      themispp::secure_session_t client("client", std::vector<uint8_t>(client_priv, client_priv+sizeof(client_priv)), &callbacks);
-      std::vector<uint8_t> msg=client.init();
-      while(!client.is_established()){
-	tr.write(msg);
-	msg=client.unwrap(tr.read());
-      }
-      tr.write(client.wrap(std::vector<uint8_t>(mes.c_str(), mes.c_str()+mes.length()+1)));
-      sput_fail_unless(strcmp(mes.c_str(), (const char*)(&(client.unwrap(tr.read()))[0]))==0, "client part", __LINE__);
-    }
-
-    void server_func(){
-      std::string mes("the test message");
-      callback callbacks;
-      transport tr(qcs, qsc);
-      themispp::secure_session_t server("server", std::vector<uint8_t>(server_priv, server_priv+sizeof(server_priv)), &callbacks);
-      while(!server.is_established()){
-	tr.write(server.unwrap(tr.read()));
-      } 
-      std::vector<uint8_t> a=server.unwrap(tr.read());
-      sput_fail_unless(strcmp(mes.c_str(), (const char*)(&a[0]))==0, "server part", __LINE__);
-      tr.write(server.wrap(a));
-    }
-
     void secure_session_test(){
-      boost::thread client_thread(&client_func);
-      boost::thread server_thread(&server_func);
-      client_thread.join();
-      server_thread.join();
+      std::string mes("the test message");
+      callback client_callbacks;
+      themispp::secure_session_t client("client", std::vector<uint8_t>(client_priv, client_priv+sizeof(client_priv)), &client_callbacks);
+
+      callback server_callbacks;
+      themispp::secure_session_t server("server", std::vector<uint8_t>(server_priv, server_priv+sizeof(server_priv)), &server_callbacks);
+
+      std::cout<<1<<std::endl;
+      std::vector<uint8_t> control_msg1=client.init();
+      std::cout<<2<<std::endl;
+      std::vector<uint8_t> control_msg2=server.unwrap(control_msg1);
+      std::cout<<3<<std::endl;
+      std::vector<uint8_t> control_msg3=client.unwrap(control_msg2);
+      std::cout<<4<<std::endl;
+      std::vector<uint8_t> control_msg4=server.unwrap(control_msg3);
+      std::cout<<5<<std::endl;
+      std::vector<uint8_t> control_msg5=client.unwrap(control_msg4);
+      std::cout<<6<<std::endl;
+      sput_fail_unless(server.is_established(), "server ready", __LINE__);
+      std::cout<<7<<std::endl;
+      sput_fail_unless(client.is_established(), "client ready", __LINE__);
+      std::cout<<8<<std::endl;
+
+      std::vector<uint8_t> msg1=client.wrap(std::vector<uint8_t>(mes.c_str(), mes.c_str()+mes.length()));
+      std::cout<<9<<std::endl;
+      std::vector<uint8_t> msg2=server.unwrap(msg1);
+      std::cout<<10<<std::endl;
+      sput_fail_unless(strcmp(mes.c_str(), (const char*)(&msg2[0]))==0, "server get message", __LINE__);
+      std::cout<<11<<std::endl;
+
+      std::vector<uint8_t> msg3=server.wrap(std::vector<uint8_t>(mes.c_str(), mes.c_str()+mes.length()));
+      std::cout<<12<<std::endl;
+      std::vector<uint8_t> msg4=client.unwrap(msg3);
+      std::cout<<13<<std::endl;
+      sput_fail_unless(strcmp(mes.c_str(), (const char*)(&msg4[0]))==0, "client get message", __LINE__);
+      std::cout<<14<<std::endl;
+
     }
     
     int run_secure_session_test(){
