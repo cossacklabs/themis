be5e842c5bd75a327c287be435a677f6151894df
andrey <andrey@cossacklabs.com>
2016-01-04 20:06:34 +0200

    Add ThemisPP tests to CircleCI

--- src/wrappers/themis/themispp/secure_session.hpp
+++ src/wrappers/themis/themispp/secure_session.hpp
@@ -22,13 +22,32 @@
 #include <themis/themis.h>
 #include "exception.hpp"
 
+#define THEMISPP_SECURE_SESSION_MAX_MESSAGE_SIZE 2048
 namespace themispp{
 
   class secure_session_callback_interface_t{
   public:
-    typedef std::vector<uint8_t> data_t; 
-    virtual const std::vector<uint8_t> get_pub_key_by_id(const data_t& id)=0;
+    typedef std::vector<uint8_t> data_t;
+    virtual const data_t get_pub_key_by_id(const data_t& id)=0;
+    virtual void send(const data_t& data){throw themispp::exception_t("send callbck not set");}
+    virtual const data_t& receive(){throw themispp::exception_t("receive callbck not set");}
   };
+
+  ssize_t send_callback(const uint8_t* data, size_t data_length, void *user_data){
+    try{
+      ((secure_session_callback_interface_t*)user_data)->send(std::vector<uint8_t>((uint8_t*)data, (uint8_t*)data+data_length));
+    }catch(...){return -1;}
+    return ssize_t(data_length);
+  }
+
+  ssize_t receive_callback(uint8_t* data, size_t data_length, void *user_data){
+    try{
+      std::vector<uint8_t> received_data=((secure_session_callback_interface_t*)user_data)->receive();
+      memcpy(data, &received_data[0], received_data.size());
+      return ssize_t(received_data.size());
+    }catch(...){}
+    return -1;
+  }
 
   int get_public_key_for_id_callback(const void *id, size_t id_length, void *key_buffer, size_t key_buffer_length, void *user_data){
     std::vector<uint8_t> pubk=((secure_session_callback_interface_t*)user_data)->get_pub_key_by_id(std::vector<uint8_t>(static_cast<const uint8_t*>(id), static_cast<const uint8_t*>(id)+id_length));
@@ -42,13 +61,16 @@ namespace themispp{
   class secure_session_t{
   public:
     typedef std::vector<uint8_t> data_t; 
-
-    secure_session_t(const std::string& id, const data_t& priv_key, secure_session_callback_interface_t* callbacks):
+    
+    secure_session_t(const data_t& id, const data_t& priv_key, secure_session_callback_interface_t* callbacks):
       _session(NULL),
       _res(0){
       _callback.get_public_key_for_id=themispp::get_public_key_for_id_callback;
+      _callback.send_data=themispp::send_callback;
+      _callback.receive_data=themispp::receive_callback;
+      _callback.state_changed=NULL;
       _callback.user_data=callbacks;
-      _session=secure_session_create(id.c_str(), id.length(), &priv_key[0], priv_key.size(), &_callback);
+      _session=secure_session_create(&id[0], id.size(), &priv_key[0], priv_key.size(), &_callback);
       if(!_session)
 	throw themispp::exception_t("secure_session_create failed");
     }
@@ -86,13 +108,32 @@ namespace themispp{
       if(secure_session_generate_connect_request(_session, NULL, &init_data_length)!=THEMIS_BUFFER_TOO_SMALL)
 	throw themispp::exception_t("secure_session_generate_connect_request (length determination) failed");
       _res.resize(init_data_length);
-      if(secure_session_generate_connect_request(_session, &_res[0], &init_data_length)!=THEMIS_SUCCESS)
+      if(secure_session_generate_connect_request(_session, &_res.front(), &init_data_length)!=THEMIS_SUCCESS)
 	throw themispp::exception_t("secure_session_generate_connect_request failed");
       return _res;
     }
     
     const bool is_established(){return secure_session_is_established(_session);}
 
+    void connect(){
+      if(secure_session_connect(_session)!=THEMIS_SUCCESS)
+	throw themispp::exception_t("secure_session_connect failed");
+    }
+
+    const data_t& receive(){
+      _res.resize(THEMISPP_SECURE_SESSION_MAX_MESSAGE_SIZE);
+      ssize_t recv_size=secure_session_receive(_session, &_res[0], _res.size());
+      if(recv_size<=0)
+	throw themispp::exception_t("secure_session_receive failed");
+      _res.resize(recv_size);
+      return _res;
+    }
+
+    void send(const data_t& data){
+      ssize_t send_size=secure_session_send(_session, &data[0], data.size());
+      if(send_size<=0)
+	throw themispp::exception_t("secure_session_send failed");
+    }
   private:
     ::secure_session_t* _session;
     ::secure_session_user_callbacks_t _callback;
