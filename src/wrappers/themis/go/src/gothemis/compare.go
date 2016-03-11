0424d0851d045d08781b0e4f7e5f7e0cffc964ed
Ignat Korchagin <ignat@cossacklabs.com>
2016-03-11 10:44:19 +0000

    Add Go secure compare

---
+++ src/wrappers/themis/go/src/gothemis/compare.go
@@ -0,0 +1,142 @@
+package gothemis
+
+/*
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <themis/error.h>
+#include <soter/soter_t.h>
+#include <themis/secure_comparator_t.h>
+
+static size_t get_comparator_ctx_size(void)
+{
+	return sizeof(secure_comparator_t);
+}
+
+static bool compare_init(void *ctx)
+{
+	return THEMIS_SUCCESS == secure_comparator_init(ctx);
+}
+
+static bool compare_append(void *ctx, const void *secret, size_t secret_length)
+{
+	return THEMIS_SUCCESS == secure_comparator_append_secret(ctx, secret, secret_length);
+}
+
+static bool compare_begin_size(void *ctx, size_t *out_len)
+{
+	THEMIS_BUFFER_TOO_SMALL == secure_comparator_begin_compare(ctx, NULL, out_len);
+}
+
+static bool compare_begin(void *ctx, void *out, size_t out_len)
+{
+	THEMIS_SCOMPARE_SEND_OUTPUT_TO_PEER == secure_comparator_begin_compare(ctx, out, &out_len);
+}
+
+static bool compare_proceed_size(void *ctx, const void *in, size_t in_len, size_t *out_len)
+{
+	THEMIS_BUFFER_TOO_SMALL == secure_comparator_proceed_compare(ctx, in, in_len, NULL, out_len);
+}
+
+static int compare_proceed(void *ctx, const void *in, size_t in_len, void *out, size_t out_len)
+{
+	return secure_comparator_proceed_compare(ctx, in, in_len, out, &out_len);
+}
+
+static int compare_result(void *ctx)
+{
+	themis_status_t res = secure_comparator_get_result(ctx);
+	
+	if (THEMIS_SCOMPARE_MATCH == res)
+	{
+		return 1;
+	}
+	
+	return (int)res;
+}
+
+*/
+import "C"
+import (
+	"unsafe"
+	"errors"
+)
+
+const (
+	COMPARE_NOT_READY = 0
+	COMPARE_NO_MATCH = -1
+	COMPARE_MATCH = 1
+)
+
+type SecureCompare struct {
+	ctx []byte
+}
+
+func New() (*SecureCompare, error) {
+	ctx := make([]byte, C.get_comparator_ctx_size())
+	
+	if ! bool(C.compare_init(unsafe.Pointer(&ctx[0]))) {
+		return nil, errors.New("Failed to create comparator object")
+	}
+	
+	return &SecureCompare{ctx}, nil
+}
+
+func (sc *SecureCompare) Append(secret []byte) error {
+	if ! bool(C.compare_append(unsafe.Pointer(&sc.ctx[0]), unsafe.Pointer(&secret[0]), C.size_t(len(secret)))) {
+		return errors.New("Failed to append secret")
+	}
+	
+	return nil
+}
+
+func (sc *SecureCompare) Begin() ([]byte, error) {
+	var outLen C.size_t
+	
+	if ! bool(C.compare_begin_size(unsafe.Pointer(&sc.ctx[0]), &outLen)) {
+		return nil, errors.New("Failed to get output size")
+	}
+	
+	out := make([]byte, outLen)
+	
+	if ! bool(C.compare_begin(unsafe.Pointer(&sc.ctx[0]), unsafe.Pointer(&out[0]), outLen)) {
+		return nil, errors.New("Failed to get compare data")
+	}
+	
+	return out, nil
+}
+
+func (sc *SecureCompare) Proceed(data []byte) ([]byte, error) {
+	var outLen C.size_t
+	
+	if ! bool(C.compare_proceed_size(unsafe.Pointer(&sc.ctx[0]), unsafe.Pointer(&data[0]), C.size_t(len(data)), &outLen)) {
+		return nil, errors.New("Failed to get output size")
+	}
+	
+	if 0 == outLen {
+		outLen++
+	}
+	
+	out := make([]byte, outLen)
+	
+	res := C.compare_proceed(unsafe.Pointer(&sc.ctx[0]), unsafe.Pointer(&data[0]), C.size_t(len(data)), unsafe.Pointer(&out[0]), outLen)
+	switch {
+		case 0 == res:
+			return nil, nil
+		case 1 == res:
+			return out, nil
+	}
+	
+	return nil, errors.New("Failed to get output")
+}
+
+func (sc *SecureCompare) Result() (int, error) {
+	res := C.compare_result(unsafe.Pointer(&sc.ctx[0]))
+	
+	switch res {
+		case COMPARE_NOT_READY, COMPARE_NO_MATCH, COMPARE_MATCH: return int(res), nil
+	}
+	
+	return COMPARE_NOT_READY, errors.New("Failed to get compare result")
+}
---
+++ src/wrappers/themis/go/src/gothemis/compare_test.go
@@ -0,0 +1,109 @@
+package gothemis
+
+import (
+    "testing"
+    _"fmt"
+)
+
+func scService(sc *SecureCompare, ch chan []byte, finCh chan int, t *testing.T) {
+	res, err := sc.Result()
+	if err != nil {
+		t.Error(err)
+		finCh <- COMPARE_NOT_READY
+		return
+	}
+	
+	for COMPARE_NOT_READY == res {
+		buf := <-ch
+		
+		buf, err := sc.Proceed(buf)
+		if err != nil {
+			t.Error(err)
+			finCh <- COMPARE_NOT_READY
+			return
+		}
+		
+		if nil != buf {
+			ch <- buf
+		}
+		
+		res, err = sc.Result()
+		if err != nil {
+			t.Error(err)
+			finCh <- COMPARE_NOT_READY
+			return
+		}
+	}
+	
+	finCh <- res
+}
+
+func compare(sec1, sec2 []byte, expected int, t *testing.T) {
+	alice, err := New()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	bob, err := New()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	err = alice.Append(sec1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	err = bob.Append(sec2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	start, err := alice.Begin()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	ch := make(chan []byte)
+	finCh := make(chan int)
+
+	go scService(bob, ch, finCh, t)
+	ch <- start
+	go scService(alice, ch, finCh, t)
+	
+	res1 := <- finCh
+	res2 := <- finCh
+	
+	if res1 != res2 {
+		t.Errorf("Results do not match: %d %d", res1, res2)
+		return
+	}
+	
+	if res1 != expected {
+		t.Errorf("Incorrect match result")
+		return
+	}
+}
+
+func TestCompare(t *testing.T) {
+	sec1, err := genRandData()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	sec2, err := genRandData()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	
+	compare(sec1, sec2, COMPARE_NO_MATCH, t);
+	compare(sec1, sec1, COMPARE_MATCH, t);
+}
+
