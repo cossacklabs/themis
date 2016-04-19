c1befe431d56c363c1d3b096c8b57b29c03e595c
vixentael <vixentael@gmail.com>
2016-04-19 03:06:17 +0300

    [themis-server] init project on swift as secure message client

---
+++ docs/examples/Themis-server/swift/SwiftThemisServerExample/SwiftThemisServerExample/SSessionClient.swift
@@ -0,0 +1,16 @@
+//
+//  SSessionClient.swift
+//  SwiftThemisServerExample
+//
+//  Created by Anastasi Voitova on 19.04.16.
+//  Copyright © 2016 CossackLabs. All rights reserved.
+//
+
+import Foundation
+
+final class SSessionClient {
+
+    func runSecureMessageCITest() {
+        // TBD
+    }
+}
---
+++ docs/examples/Themis-server/swift/SwiftThemisServerExample/SwiftThemisServerExample/SwiftThemisServerExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftThemisServerExample-Bridging-Header.h
+//  SwiftThemisServerExample
+//
+//  Created by Anastasi Voitova on 19.04.16.
+//  Copyright © 2016 CossackLabs. All rights reserved.
+//
+
+#ifndef SwiftThemisServerExample_Bridging_Header_h
+#define SwiftThemisServerExample_Bridging_Header_h
+
+    #import <objcthemis/objcthemis.h>
+
+#endif /* SwiftThemisServerExample_Bridging_Header_h */
