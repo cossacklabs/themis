8704aa5ba96573ea30de17cd16401e1238f24fc2
Ignat Korchagin <ignat@cossacklabs.com>
2016-05-24 15:03:17 +0100

    Add themis_jni target to Makefile

---
+++ jni/themis_jni.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2015 Cossack Labs Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+THEMIS_JNI_SRC = $(wildcard jni/*.c)
+
+THEMIS_JNI_OBJ = $(patsubst jni/%.c,$(OBJ_PATH)/jni/%.o, $(THEMIS_JNI_SRC))
+
+THEMIS_JNI_BIN = themis_jni
+
+ifeq ($(JDK_INCLUDE_PATH),)
+	JDK_INCLUDE_PATH=$(JAVA_HOME)/include
+endif
+
+$(OBJ_PATH)/jni/%.o: jni/%.c
+	@mkdir -p $(@D)
+	@$(CC) $(CFLAGS) -I$(JDK_INCLUDE_PATH) -c $< -o $@
