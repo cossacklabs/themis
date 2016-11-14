f7a8b5347936180ce61ef0880b6582a781bdfb5d
lagovas <lagovas.lagovas@gmail.com>
2016-11-14 16:13:15 +0200

    fix mistake in error message

--- gothemis/cell/cell.go
+++ gothemis/cell/cell.go
@@ -66,7 +66,7 @@ static bool encrypt(const void *key, size_t key_len, const void *data, size_t da
 static bool get_unprotect_size(const void *key, size_t key_len, const void *prot, size_t prot_len, const void *add, size_t add_len, const void *context, size_t context_len, int mode, size_t *dec_len)
 {
 	themis_status_t res = THEMIS_FAIL;
-	
+
 	switch (mode)
 	{
 	case MODE_SEAL:
@@ -96,7 +96,7 @@ static bool get_unprotect_size(const void *key, size_t key_len, const void *prot
 static bool decrypt(const void *key, size_t key_len, const void *prot, size_t prot_len, const void *add, size_t add_len, const void *context, size_t context_len, int mode, void *dec, size_t dec_len)
 {
 	themis_status_t res = THEMIS_FAIL;
-	
+
 	switch (mode)
 	{
 	case MODE_SEAL:
@@ -131,13 +131,13 @@ import (
 )
 
 const (
-	CELL_MODE_SEAL = 0
-	CELL_MODE_TOKEN_PROTECT = 1
+	CELL_MODE_SEAL            = 0
+	CELL_MODE_TOKEN_PROTECT   = 1
 	CELL_MODE_CONTEXT_IMPRINT = 2
 )
 
 type SecureCell struct {
-	key []byte
+	key  []byte
 	mode int
 }
 
@@ -149,135 +149,135 @@ func (sc *SecureCell) Protect(data []byte, context []byte) ([]byte, []byte, erro
 	if (sc.mode < CELL_MODE_SEAL) || (sc.mode > CELL_MODE_CONTEXT_IMPRINT) {
 		return nil, nil, errors.New("Invalid mode specified")
 	}
-	
+
 	if nil == sc.key {
 		return nil, nil, errors.New("Master key was not provided")
 	}
-	
+
 	if nil == data {
 		return nil, nil, errors.New("Data was not provided")
 	}
-	
+
 	if CELL_MODE_CONTEXT_IMPRINT == sc.mode {
 		if nil == context {
 			return nil, nil, errors.New("Context is mandatory for context imprint mode")
 		}
 	}
-	
+
 	var ctx unsafe.Pointer
 	var ctxLen C.size_t
-	
+
 	if nil != context {
 		ctx = unsafe.Pointer(&context[0])
 		ctxLen = C.size_t(len(context))
 	}
-	
+
 	var encLen, addLen C.size_t
-	
-	if ! bool(C.get_protect_size(unsafe.Pointer(&sc.key[0]),
-			C.size_t(len(sc.key)),
-			unsafe.Pointer(&data[0]),
-			C.size_t(len(data)),
-			ctx,
-			ctxLen,
-			C.int(sc.mode),
-			&encLen,
-			&addLen)) {
-				return nil, nil, errors.New("Failed to get ouput size")
-			}
-			
+
+	if !bool(C.get_protect_size(unsafe.Pointer(&sc.key[0]),
+		C.size_t(len(sc.key)),
+		unsafe.Pointer(&data[0]),
+		C.size_t(len(data)),
+		ctx,
+		ctxLen,
+		C.int(sc.mode),
+		&encLen,
+		&addLen)) {
+		return nil, nil, errors.New("Failed to get output size")
+	}
+
 	var addData []byte
 	var add unsafe.Pointer
-		
+
 	encData := make([]byte, encLen, encLen)
-	if (addLen > 0) {
+	if addLen > 0 {
 		addData = make([]byte, addLen, addLen)
 		add = unsafe.Pointer(&addData[0])
 	}
-	
-	if ! bool(C.encrypt(unsafe.Pointer(&sc.key[0]),
-			C.size_t(len(sc.key)),
-			unsafe.Pointer(&data[0]),
-			C.size_t(len(data)),
-			ctx,
-			ctxLen,
-			C.int(sc.mode),
-			unsafe.Pointer(&encData[0]),
-			encLen,
-			add,
-			addLen)) {
-				return nil, nil, errors.New("Failed to protect data")
-			}
-			
-	return encData, addData, nil 		
+
+	if !bool(C.encrypt(unsafe.Pointer(&sc.key[0]),
+		C.size_t(len(sc.key)),
+		unsafe.Pointer(&data[0]),
+		C.size_t(len(data)),
+		ctx,
+		ctxLen,
+		C.int(sc.mode),
+		unsafe.Pointer(&encData[0]),
+		encLen,
+		add,
+		addLen)) {
+		return nil, nil, errors.New("Failed to protect data")
+	}
+
+	return encData, addData, nil
 }
 
 func (sc *SecureCell) Unprotect(protectedData []byte, additionalData []byte, context []byte) ([]byte, error) {
 	if (sc.mode < CELL_MODE_SEAL) || (sc.mode > CELL_MODE_CONTEXT_IMPRINT) {
 		return nil, errors.New("Invalid mode specified")
 	}
-	
+
 	if nil == sc.key {
 		return nil, errors.New("Master key was not provided")
 	}
-	
+
 	if nil == protectedData {
 		return nil, errors.New("Data was not provided")
 	}
-	
+
 	if CELL_MODE_CONTEXT_IMPRINT == sc.mode {
 		if nil == context {
 			return nil, errors.New("Context is mandatory for context imprint mode")
 		}
 	}
-	
+
 	if CELL_MODE_TOKEN_PROTECT == sc.mode {
 		if nil == additionalData {
 			return nil, errors.New("Additional data is mandatory for token protect mode")
 		}
 	}
-	
+
 	var add, ctx unsafe.Pointer
 	var addLen, ctxLen C.size_t
-	
+
 	if nil != additionalData {
 		add = unsafe.Pointer(&additionalData[0])
 		addLen = C.size_t(len(additionalData))
 	}
-	
+
 	if nil != context {
 		ctx = unsafe.Pointer(&context[0])
 		ctxLen = C.size_t(len(context))
 	}
-	
+
 	var decLen C.size_t
-	if ! bool(C.get_unprotect_size(unsafe.Pointer(&sc.key[0]),
-			C.size_t(len(sc.key)),
-			unsafe.Pointer(&protectedData[0]),
-			C.size_t(len(protectedData)),
-			add,
-			addLen,
-			ctx,
-			ctxLen,
-			C.int(sc.mode),
-			&decLen)) {
-				return nil, errors.New("Failed to get ouput size");
-			}
-			
+	if !bool(C.get_unprotect_size(unsafe.Pointer(&sc.key[0]),
+		C.size_t(len(sc.key)),
+		unsafe.Pointer(&protectedData[0]),
+		C.size_t(len(protectedData)),
+		add,
+		addLen,
+		ctx,
+		ctxLen,
+		C.int(sc.mode),
+		&decLen)) {
+		return nil, errors.New("Failed to get output size")
+	}
+
 	decData := make([]byte, decLen, decLen)
-	if ! bool(C.decrypt(unsafe.Pointer(&sc.key[0]),
-			C.size_t(len(sc.key)),
-			unsafe.Pointer(&protectedData[0]),
-			C.size_t(len(protectedData)),
-			add,
-			addLen,
-			ctx,
-			ctxLen,
-			C.int(sc.mode),
-			unsafe.Pointer(&decData[0]),
-			decLen)) {
-				return nil, errors.New("Failed to unprotect data");
-			}
-			
-	return decData, nil		
+	if !bool(C.decrypt(unsafe.Pointer(&sc.key[0]),
+		C.size_t(len(sc.key)),
+		unsafe.Pointer(&protectedData[0]),
+		C.size_t(len(protectedData)),
+		add,
+		addLen,
+		ctx,
+		ctxLen,
+		C.int(sc.mode),
+		unsafe.Pointer(&decData[0]),
+		decLen)) {
+		return nil, errors.New("Failed to unprotect data")
+	}
+
+	return decData, nil
 }
