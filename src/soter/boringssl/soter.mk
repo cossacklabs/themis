234c8dbe16b2d12697c0d866999b54f2f8a18206
andrey <andrey@cossacklabs.com>
2017-01-16 16:07:40 +0200

    Add coveralls.io support

--- src/soter/boringssl/soter.mk
+++ src/soter/boringssl/soter.mk
@@ -23,6 +23,6 @@ ifneq ($(CRYPTO_ENGINE_INCLUDE_PATH),)
 	CFLAGS += -I$(CRYPTO_ENGINE_INCLUDE_PATH)
 endif
 ifneq ($(CRYPTO_ENGINE_LIB_PATH),)
-	LDFLAGS += -L$(CRYPTO_ENGINE_LIB_PATH)
+	LDFLAGS = -L$(CRYPTO_ENGINE_LIB_PATH) $(LDFLAGS)
 endif
-LDFLAGS += -lcrypto -ldecrepit -lpthread
+LDFLAGS = -lcrypto -ldecrepit -lpthread $(LDFLAGS)
