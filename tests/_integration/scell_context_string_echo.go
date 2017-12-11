6a8303676fe888f314d01c67ef0c5805bb9fc36c
vixentael <vixentael@users.noreply.github.com>
2017-12-11 13:11:55 +0200

    one liners to test secure cell (ruby and go currently) (#260)

---
+++ tests/_integration/scell_context_string_echo.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/cossacklabs/themis/gothemis/cell"
+	"os"
+)
+
+func main() {
+	args := os.Args
+
+	if len(args) != 5 {
+		fmt.Printf("usage %s <command: enc | dec > <key> <message> <context>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	command := args[1]
+	key := args[2]
+	message := args[3]
+	context := []byte(args[4])
+
+	sc := cell.New([]byte(key), cell.CELL_MODE_CONTEXT_IMPRINT)
+
+	if "enc" == command {
+		encData, _, err := sc.Protect([]byte(message), context)
+		if nil != err {
+			fmt.Println(os.Stderr, "Error encrypting message")
+			os.Exit(1)
+		}
+		fmt.Println(base64.StdEncoding.EncodeToString(encData))
+
+	} else if "dec" == command {
+		decoded_message, err := base64.StdEncoding.DecodeString(message)
+		if nil != err {
+			fmt.Println(os.Stderr, "Error decoding message")
+			os.Exit(1)
+		}
+		decData, err := sc.Unprotect(decoded_message, nil, context)
+		if nil != err {
+			fmt.Println(os.Stderr, "Error decrypting message")
+			os.Exit(1)
+		}
+		fmt.Println(string(decData[:]))
+
+	} else {
+		fmt.Println(os.Stderr, "Wrong command, use \"enc\" or \"dec\"")
+		os.Exit(1)
+	}
+}
---
+++ tests/_integration/scell_token_string_echo.rb
@@ -0,0 +1,50 @@
+#
+# Copyright (c) 2017 Cossack Labs Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rubythemis'
+require 'base64'
+
+input_args = ARGV
+
+if input_args.length < 3 || input_args.length > 4 
+	STDERR.puts "Usage: <command: enc | dec > <key> <message,token> <context (optional)> \n"
+	exit 1
+end
+
+command = input_args[0]
+key = input_args[1].dup
+message, token = input_args[2].split(",")
+context = nil
+
+if input_args.length == 4
+	context = input_args[3].dup
+end
+
+scell = Themis::Scell.new(key, Themis::Scell::TOKEN_PROTECT_MODE)
+
+if command == "enc"
+	encr_message, token = scell.encrypt(message, context)
+	puts Base64.strict_encode64(encr_message) + "," + Base64.strict_encode64(token)
+elsif command == "dec"
+	decr_message = scell.decrypt([Base64.decode64(message), Base64.decode64(token)], context)
+	puts decr_message
+else
+	STDERR.puts "Wrong command, use \"enc\" or \"dec\""
+	exit 1
+end
