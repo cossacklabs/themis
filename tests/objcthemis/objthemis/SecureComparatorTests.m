8142b43fde72a69eed6712df8c5a2f020263a73c
Vixentael <vixentael@gmail.com>
2018-01-19 17:47:39 +0200

    tests clean up

--- tests/objcthemis/objthemis/SecureComparatorTests.m
+++ tests/objcthemis/objthemis/SecureComparatorTests.m
@@ -19,26 +19,6 @@
 
 @implementation SecureComparatorTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-/*
- alice = scomparator.SComparator(self.message)
- bob = scomparator.SComparator(self.message)
- data = alice.begin_compare()
- while not (alice.is_compared() and bob.is_compared()):
- data = alice.proceed_compare(bob.proceed_compare(data))
- self.assertTrue(alice.is_equal())
- self.assertTrue(bob.is_equal())
- */
-
 - (void)testSecureComparatorEqualMessage {
     NSString * sharedSecret = @"shared secret";
     NSData * sharedSecretData = [sharedSecret dataUsingEncoding:NSUTF8StringEncoding];
