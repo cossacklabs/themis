fa101f7d4d523ab4568c701022cd8cd2adafb3c8
Vixentael <vixentael@gmail.com>
2018-01-29 18:16:29 +0200

    fix functions style

--- tests/_integration/utils.sh
+++ tests/_integration/utils.sh
@@ -4,11 +4,11 @@ export TOP_PID=$$
 trap "exit 1" TERM
 export status=0
 
-function fail {
+function fail () {
     printf "\033[1m\033[31m ${2} fail \x1b[0m\n"
 }
 export -f fail
-function success {
+function success () {
     printf "\033[1m\033[32m ${2} success \x1b[0m\n"
 }
 export -f success
