16b1ee636908ed588a308798002505bf2fbf2387
vixentael <vixentael@users.noreply.github.com>
2018-10-10 15:58:38 +0300

    jsthemis update (support new versions of NaN and Nodejs) (#327)
    * NaN ’NewInstance’ API fix (#326)
    
    * update examples, update jsthemis.package json

--- docs/examples/js/secure_session_client.js
+++ docs/examples/js/secure_session_client.js
@@ -1,12 +1,12 @@
 var net = require('net');
 var themis = require('jsthemis');
 
-session = new themis.SecureSession(new Buffer("client"), new Buffer("UkVDMgAAAC3DZR2qAEbvO092R/IKXBttnf9dVSU65R+Fb4eNoyxxlzn2n4GR","base64"), function(id){
+session = new themis.SecureSession(new Buffer.from("client"), new Buffer.from("UkVDMgAAAC3DZR2qAEbvO092R/IKXBttnf9dVSU65R+Fb4eNoyxxlzn2n4GR","base64"), function(id){
     if(id.toString()=="server".toString()){
-	return new Buffer("VUVDMgAAAC30/vs+AwciK6egi82A9TkTydVuOzMFsJ9AkA0gMGyNH0tSu5Bk", "base64");
+	return new Buffer.from("VUVDMgAAAC30/vs+AwciK6egi82A9TkTydVuOzMFsJ9AkA0gMGyNH0tSu5Bk", "base64");
     }
     else if(id.toString()=="client".toString())
-	return new Buffer("VUVDMgAAAC15KNjgAr1DQEw+So1oztUarO4Jw/CGgyehBRCbOxbpHrPBKO7s", "base64");
+	return new Buffer.from("VUVDMgAAAC15KNjgAr1DQEw+So1oztUarO4Jw/CGgyehBRCbOxbpHrPBKO7s", "base64");
 });
 
 retry_count=5;
@@ -25,7 +25,7 @@ client.on('data', function(data) {
 	if(d != undefined)
 	    console.log(d.toString());
 	if(retry_count--)
-	    client.write(session.wrap(new Buffer("Hello server!!!")));
+	    client.write(session.wrap(new Buffer.from("Hello server!!!")));
 	else
 	    client.destroy();
     }
