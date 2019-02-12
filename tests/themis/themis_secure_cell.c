fc33568ca3b84b9c5ff2b93edc255db844609731
ilammy <a.lozovsky@gmail.com>
2019-02-12 23:21:56 +0200

    Strict function prototypes in C code
    Recently added "-Wstrict-prototypes" has found a bunch of functions
    (in unit tests) which do not have strict prototypes. In C functions
    that have "()" argument lists and do not have an explicit prototype
    are allowed to accept arbitrary amount of arguments.
    
    Ensure that all nullary functions have a "(void)" argument list for
    consistency with many other places which already use this spelling.

--- tests/themis/themis_secure_cell.c
+++ tests/themis/themis_secure_cell.c
@@ -32,7 +32,7 @@ static char passwd[] = "password";
 static char message[] = "secure cell test message by Mnatsakanov Andrey from Cossack Labs";
 static char user_context[] = "secure cell user context";
 
-static int secure_cell_seal()
+static int secure_cell_seal(void)
 {
     uint8_t* encrypted_message;
     size_t encrypted_message_length = 0;
@@ -96,7 +96,7 @@ static int secure_cell_seal()
     return 0;
 }
 
-static int secure_cell_token_protect()
+static int secure_cell_token_protect(void)
 {
     uint8_t* encrypted_message;
     size_t encrypted_message_length = 0;
@@ -188,7 +188,7 @@ static int secure_cell_token_protect()
     return 0;
 }
 
-static int secure_cell_context_imprint()
+static int secure_cell_context_imprint(void)
 {
     uint8_t* encrypted_message;
     size_t encrypted_message_length = 0;
@@ -258,7 +258,7 @@ static int secure_cell_context_imprint()
     return 0;
 }
 
-static void secure_cell_test()
+static void secure_cell_test(void)
 {
     testsuite_fail_if(secure_cell_seal(), "secure cell seal mode");
     testsuite_fail_if(secure_cell_token_protect(), "secure cell token protect mode");
@@ -878,7 +878,7 @@ static void secure_cell_context_corruption(void)
     secure_cell_context_corruption_token_protect();
 }
 
-void run_secure_cell_test()
+void run_secure_cell_test(void)
 {
     testsuite_enter_suite("secure cell: basic flow");
     testsuite_run_test(secure_cell_test);
