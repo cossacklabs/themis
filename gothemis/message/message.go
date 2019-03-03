742b6550ba1d0c3c034f15494985a169e1bb3ae4
Alexei Lozovsky <a.lozovsky@gmail.com>
2019-03-03 14:51:56 +0200

    Use new Secure Message API in GoThemis (#400)
    * Include correct header for keygen
    
    Key generation functions are now exported by <themis/secure_keygen.h>,
    not <themis/secure_message.h>. Include the correct header if we do not
    want to use <themis/themis.h>.
    
    * Use new Secure Message API in GoThemis
    
    Exactly what it says on the tin. Get rid of the deprecation warnings
    and use the new API instead. It's hard to use proper enums in Go-to-C
    interop so just use integer constants, that's good enough.
    
    Keep the old API naming for now. We'll update it later.
    
    * Idiomatic enumeration
    
    Use name casing with more correct Go code style. Also use "iota"
    instead of explicit integer value enumeration. Now that we use it
    replace simple defines with enums.

--- gothemis/message/message.go
+++ gothemis/message/message.go
@@ -9,33 +9,52 @@ package message
 #include <themis/themis_error.h>
 #include <themis/secure_message.h>
 
-static bool get_message_size(const void *priv, size_t priv_len, const void *public, size_t pub_len, const void *message, size_t message_len, bool is_wrap, size_t *out_len)
-{
-	themis_status_t res;
+enum {
+	SecureMessageEncrypt,
+	SecureMessageDecrypt,
+	SecureMessageSign,
+	SecureMessageVerify,
+};
 
-	if (is_wrap)
-	{
-		res = themis_secure_message_wrap(priv, priv_len, public, pub_len, message, message_len, NULL, out_len);
-	}
-	else
-	{
-		res = themis_secure_message_unwrap(priv, priv_len, public, pub_len, message, message_len, NULL, out_len);
+static bool get_message_size(const void *priv, size_t priv_len, const void *public, size_t pub_len, const void *message, size_t message_len, int mode, size_t *out_len)
+{
+	themis_status_t res = THEMIS_NOT_SUPPORTED;
+
+	switch (mode) {
+	case SecureMessageEncrypt:
+		res = themis_secure_message_encrypt(priv, priv_len, public, pub_len, message, message_len, NULL, out_len);
+		break;
+	case SecureMessageDecrypt:
+		res = themis_secure_message_decrypt(priv, priv_len, public, pub_len, message, message_len, NULL, out_len);
+		break;
+	case SecureMessageSign:
+		res = themis_secure_message_sign(priv, priv_len, message, message_len, NULL, out_len);
+		break;
+	case SecureMessageVerify:
+		res = themis_secure_message_verify(public, pub_len, message, message_len, NULL, out_len);
+		break;
 	}
 
 	return THEMIS_BUFFER_TOO_SMALL == res;
 }
 
-static bool process(const void *priv, size_t priv_len, const void *public, size_t pub_len, const void *message, size_t message_len, bool is_wrap, void *out, size_t out_len)
+static bool process(const void *priv, size_t priv_len, const void *public, size_t pub_len, const void *message, size_t message_len, int mode, void *out, size_t out_len)
 {
-	themis_status_t res;
+	themis_status_t res = THEMIS_NOT_SUPPORTED;
 
-	if (is_wrap)
-	{
-		res = themis_secure_message_wrap(priv, priv_len, public, pub_len, message, message_len, out, &out_len);
-	}
-	else
-	{
-		res = themis_secure_message_unwrap(priv, priv_len, public, pub_len, message, message_len, out, &out_len);
+	switch (mode) {
+	case SecureMessageEncrypt:
+		res = themis_secure_message_encrypt(priv, priv_len, public, pub_len, message, message_len, out, &out_len);
+		break;
+	case SecureMessageDecrypt:
+		res = themis_secure_message_decrypt(priv, priv_len, public, pub_len, message, message_len, out, &out_len);
+		break;
+	case SecureMessageSign:
+		res = themis_secure_message_sign(priv, priv_len, message, message_len, out, &out_len);
+		break;
+	case SecureMessageVerify:
+		res = themis_secure_message_verify(public, pub_len, message, message_len, out, &out_len);
+		break;
 	}
 
 	return THEMIS_SUCCESS == res;
@@ -49,6 +68,13 @@ import (
 	"unsafe"
 )
 
+const (
+	SecureMessageEncrypt = iota
+	SecureMessageDecrypt
+	SecureMessageSign
+	SecureMessageVerify
+)
+
 type SecureMessage struct {
 	private    *keys.PrivateKey
 	peerPublic *keys.PublicKey
@@ -58,7 +84,7 @@ func New(private *keys.PrivateKey, peerPublic *keys.PublicKey) *SecureMessage {
 	return &SecureMessage{private, peerPublic}
 }
 
-func messageProcess(private *keys.PrivateKey, peerPublic *keys.PublicKey, message []byte, is_wrap bool) ([]byte, error) {
+func messageProcess(private *keys.PrivateKey, peerPublic *keys.PublicKey, message []byte, mode int) ([]byte, error) {
 	if nil == message || 0 == len(message) {
 		return nil, errors.New("No message was provided")
 	}
@@ -87,7 +113,7 @@ func messageProcess(private *keys.PrivateKey, peerPublic *keys.PublicKey, messag
 		pubLen,
 		unsafe.Pointer(&message[0]),
 		C.size_t(len(message)),
-		C.bool(is_wrap),
+		C.int(mode),
 		&output_length)) {
 		return nil, errors.New("Failed to get output size")
 	}
@@ -99,15 +125,21 @@ func messageProcess(private *keys.PrivateKey, peerPublic *keys.PublicKey, messag
 		pubLen,
 		unsafe.Pointer(&message[0]),
 		C.size_t(len(message)),
-		C.bool(is_wrap),
+		C.int(mode),
 		unsafe.Pointer(&output[0]),
 		output_length)) {
-		if is_wrap {
-			return nil, errors.New("Failed to wrap message")
-		} else {
-			return nil, errors.New("Failed to unwrap message")
+		switch mode {
+		case SecureMessageEncrypt:
+			return nil, errors.New("Failed to encrypt message")
+		case SecureMessageDecrypt:
+			return nil, errors.New("Failed to decrypt message")
+		case SecureMessageSign:
+			return nil, errors.New("Failed to sign message")
+		case SecureMessageVerify:
+			return nil, errors.New("Failed to verify message")
+		default:
+			return nil, errors.New("Failed to process message")
 		}
-
 	}
 
 	return output, nil
@@ -121,7 +153,7 @@ func (sm *SecureMessage) Wrap(message []byte) ([]byte, error) {
 	if nil == sm.peerPublic || 0 == len(sm.peerPublic.Value) {
 		return nil, errors.New("Peer public key was not provided")
 	}
-	return messageProcess(sm.private, sm.peerPublic, message, true)
+	return messageProcess(sm.private, sm.peerPublic, message, SecureMessageEncrypt)
 }
 
 func (sm *SecureMessage) Unwrap(message []byte) ([]byte, error) {
@@ -132,7 +164,7 @@ func (sm *SecureMessage) Unwrap(message []byte) ([]byte, error) {
 	if nil == sm.peerPublic || 0 == len(sm.peerPublic.Value) {
 		return nil, errors.New("Peer public key was not provided")
 	}
-	return messageProcess(sm.private, sm.peerPublic, message, false)
+	return messageProcess(sm.private, sm.peerPublic, message, SecureMessageDecrypt)
 }
 
 func (sm *SecureMessage) Sign(message []byte) ([]byte, error) {
@@ -140,7 +172,7 @@ func (sm *SecureMessage) Sign(message []byte) ([]byte, error) {
 		return nil, errors.New("Private key was not provided")
 	}
 
-	return messageProcess(sm.private, nil, message, true)
+	return messageProcess(sm.private, nil, message, SecureMessageSign)
 }
 
 func (sm *SecureMessage) Verify(message []byte) ([]byte, error) {
@@ -148,5 +180,5 @@ func (sm *SecureMessage) Verify(message []byte) ([]byte, error) {
 		return nil, errors.New("Peer public key was not provided")
 	}
 
-	return messageProcess(nil, sm.peerPublic, message, false)
+	return messageProcess(nil, sm.peerPublic, message, SecureMessageVerify)
 }
