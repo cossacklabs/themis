97550a8d58e04c74b2e9bdb100aeac2d04bceceb
Vixentael <vixentael@gmail.com>
2019-03-06 00:06:37 +0200

    empty high-level header

--- src/wrappers/themis/Obj-C/objcthemis/objcthemis.h
+++ src/wrappers/themis/Obj-C/objcthemis/objcthemis.h
@@ -1,14 +1,6 @@
 #ifndef _OBJCTHEMIS_
     #define _OBJCTHEMIS_
 
-	#import <objcthemis/scell_seal.h>
-	#import <objcthemis/scell_token.h>
-	#import <objcthemis/scell_context_imprint.h>
-	#import <objcthemis/skeygen.h>
-	#import <objcthemis/smessage.h>
-	#import <objcthemis/scomparator.h>
-	#import <objcthemis/ssession.h>
-	#import <objcthemis/serror.h>
 
 #endif /* _OBJCTHEMIS_ */
 
