ae549e49161dc143aeec59b8132b4c7e2e2c6d97
ilammy <a.lozovsky@gmail.com>
2019-03-11 20:21:53 +0200

    Spell ID as "ID", not "Id"
    golint warns us about incorrect spelling, fix it where possible.
    
    Don't rename GetRemoteId and GetPublicKeyForId methods to avoid
    breaking backward compatibility.

--- gothemis/session/session_test.go
+++ gothemis/session/session_test.go
@@ -14,13 +14,13 @@ type testCallbacks struct {
 	b *keys.Keypair
 }
 
-var clientId = []byte("client a")
-var serverId = []byte("client b")
+var clientID = []byte("client a")
+var serverID = []byte("client b")
 
 func (clb *testCallbacks) GetPublicKeyForId(ss *SecureSession, id []byte) *keys.PublicKey {
-	if bytes.Equal(clientId, id){
+	if bytes.Equal(clientID, id){
 		return clb.a.Public
-	} else if bytes.Equal(serverId, id){
+	} else if bytes.Equal(serverID, id){
 		return clb.b.Public
 	}
 	return nil
@@ -80,12 +80,12 @@ func clientService(client *SecureSession, ch chan []byte, finCh chan int, t *tes
 			return
 		}
 
-		remoteId, err := client.GetRemoteId()
+		remoteID, err := client.GetRemoteId()
 		if err != nil{
 			t.Error(err)
 			return
 		}
-		if !bytes.Equal(remoteId, serverId){
+		if !bytes.Equal(remoteID, serverID){
 			t.Error("incorrect remote id")
 			return
 		}
@@ -133,12 +133,12 @@ func serverService(server *SecureSession, ch chan []byte, finCh chan int, t *tes
 			t.Error(err)
 			return
 		}
-		remoteId, err := server.GetRemoteId()
+		remoteID, err := server.GetRemoteId()
 		if err != nil{
 			t.Error(err)
 			return
 		}
-		if !bytes.Equal(remoteId, clientId){
+		if !bytes.Equal(remoteID, clientID){
 			t.Error("incorrect remote id")
 			return
 		}
@@ -179,13 +179,13 @@ func testSession(keytype int, t *testing.T) {
 
 	emptyKey := keys.PrivateKey{[]byte{}}
 
-	client, err := New(clientId, nil, clb)
+	client, err := New(clientID, nil, clb)
 	if nil == err {
 		t.Error("Creating Secure session object with empty private key")
 		return
 	}
 
-	client, err = New(clientId, &emptyKey, clb)
+	client, err = New(clientID, &emptyKey, clb)
 	if nil == err {
 		t.Error("Creating Secure session object with empty private key")
 		return
@@ -203,13 +203,13 @@ func testSession(keytype int, t *testing.T) {
 		return
 	}
 
-	client, err = New(clientId, kpa.Private, clb)
+	client, err = New(clientID, kpa.Private, clb)
 	if nil != err {
 		t.Error(err)
 		return
 	}
 
-	server, err := New(serverId, kpb.Private, clb)
+	server, err := New(serverID, kpb.Private, clb)
 	if nil != err {
 		t.Error(err)
 		return
