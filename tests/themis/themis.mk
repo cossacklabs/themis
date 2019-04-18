31c76a1a31e6903969bf81c22f98314070cc359e
ilammy <a.lozovsky@gmail.com>
2019-04-18 03:24:13 +0300

    Really link Emscripten statically
    While linking with "-lthemis" and *.so files seems to work at first,
    that's not true for the case when Themis is already installed in the
    system. Then emcc for some reason ignores the 'shared' Wasm libraries
    in the build directory and tries linking against the native system
    libraries (and failes).
    
    Use the correct static linkage for Wasm targets: build the static libs,
    and link against them instead of shared objects. However, continue
    using shared objects for everything else.

--- tests/themis/themis.mk
+++ tests/themis/themis.mk
@@ -31,9 +31,10 @@ FMT_FIXUP += $(patsubst %,$(OBJ_PATH)/%.fmt_fixup, $(THEMIS_TEST_FMT))
 FMT_CHECK += $(patsubst %,$(OBJ_PATH)/%.fmt_check, $(THEMIS_TEST_FMT))
 
 ifdef IS_EMSCRIPTEN
+# Emscripten does not support dynamic linkage, do a static build for it.
 THEMIS_TEST_LDFLAGS += -s SINGLE_FILE=1
-endif
-
+$(THEMIS_TEST_BIN): $(THEMIS_STATIC)
+else
 # Link dynamically against the Themis library in the build directory,
 # not the one in the standard system paths (if any).
 # We also need to link against Soter explicitly because of private imports.
@@ -41,14 +42,12 @@ THEMIS_TEST_LDFLAGS += -L$(BIN_PATH) -lthemis -lsoter
 ifdef IS_LINUX
 THEMIS_TEST_LDFLAGS += -Wl,-rpath,$(abspath $(BIN_PATH))
 endif
-# Emscripten still links statically, provide all dependencies for it
-ifdef IS_EMSCRIPTEN
-THEMIS_TEST_LDFLAGS += $(CRYPTO_ENGINE_LDFLAGS)
+$(THEMIS_TEST_BIN): $(BIN_PATH)/$(LIBTHEMIS_SO)
 endif
 
 $(THEMIS_TEST_BIN): CMD = $(CC) -o $@ $(filter %.o %.a, $^) $(LDFLAGS) $(THEMIS_TEST_LDFLAGS)
 
-$(THEMIS_TEST_BIN): $(THEMIS_TEST_OBJ) $(COMMON_TEST_OBJ) $(BIN_PATH)/$(LIBTHEMIS_SO)
+$(THEMIS_TEST_BIN): $(THEMIS_TEST_OBJ) $(COMMON_TEST_OBJ)
 	@mkdir -p $(@D)
 	@echo -n "link "
 	@$(BUILD_CMD)
--- tests/themispp/themispp.mk
+++ tests/themispp/themispp.mk
@@ -24,6 +24,11 @@ THEMISPP_TEST_FMT = $(THEMISPP_TEST_SOURCES) $(THEMISPP_TEST_HEADERS)
 FMT_FIXUP += $(patsubst %,$(OBJ_PATH)/%.fmt_fixup, $(THEMISPP_TEST_FMT))
 FMT_CHECK += $(patsubst %,$(OBJ_PATH)/%.fmt_check, $(THEMISPP_TEST_FMT))
 
+ifdef IS_EMSCRIPTEN
+# Emscripten does not support dynamic linkage, do a static build for it.
+THEMISPP_TEST_LDFLAGS += -s SINGLE_FILE=1
+$(TEST_BIN_PATH)/themispp_test: $(THEMIS_STATIC)
+else
 # Link dynamically against Themis library in the build directory,
 # not the one in the standard system paths (if any).
 # We also need to link against Soter explicitly because of private imports.
@@ -31,14 +36,12 @@ THEMISPP_TEST_LDFLAGS += -L$(BIN_PATH) -lthemis -lsoter
 ifdef IS_LINUX
 THEMISPP_TEST_LDFLAGS += -Wl,-rpath,$(abspath $(BIN_PATH))
 endif
-# Emscripten still links statically, provide all dependencies for it
-ifdef IS_EMSCRIPTEN
-THEMISPP_TEST_LDFLAGS += $(CRYPTO_ENGINE_LDFLAGS)
+$(TEST_BIN_PATH)/themispp_test: $(BIN_PATH)/$(LIBTHEMIS_SO)
 endif
 
 $(TEST_BIN_PATH)/themispp_test: CMD = $(CXX) -o $@ $(filter %.o %.a, $^) $(LDFLAGS) $(THEMISPP_TEST_LDFLAGS)
 
-$(TEST_BIN_PATH)/themispp_test: $(THEMISPP_TEST_OBJ) $(COMMON_TEST_OBJ) $(BIN_PATH)/$(LIBTHEMIS_SO)
+$(TEST_BIN_PATH)/themispp_test: $(THEMISPP_TEST_OBJ) $(COMMON_TEST_OBJ)
 	@echo -n "link "
 	@$(BUILD_CMD)
 
