c13df5b100e53ff1be604fe2e3c5e60dc4818339
ilammy <alexei@cossacklabs.com>
2019-05-21 16:45:52 +0300

    Safety checks in Soter container accessors
    Now that we have replaced macros with functions, we can make them more
    resilient to invalid input. Check pointer parameters for null, return
    errors where appropriate. Also avoid integer overflow when computing
    data sizes.

--- src/soter/soter_container.c
+++ src/soter/soter_container.c
@@ -16,11 +16,17 @@
 
 #include "soter/soter_container.h"
 
+#include <limits.h>
+
 #include "soter/portable_endian.h"
 #include "soter/soter_crc32.h"
 
 soter_status_t soter_update_container_checksum(soter_container_hdr_t* hdr)
 {
+    if (!hdr) {
+        return SOTER_INVALID_PARAMETER;
+    }
+
     hdr->crc = 0;
     hdr->crc = htobe32(soter_crc32(hdr, be32toh(hdr->size)));
 
@@ -30,11 +36,16 @@ soter_status_t soter_update_container_checksum(soter_container_hdr_t* hdr)
 soter_status_t soter_verify_container_checksum(const soter_container_hdr_t* hdr)
 {
     uint32_t dummy_crc = 0;
-    soter_crc32_t crc = soter_crc32_create();
+    soter_crc32_t crc;
 
+    if (!hdr) {
+        return SOTER_INVALID_PARAMETER;
+    }
+
+    crc = soter_crc32_create();
     soter_crc32_update(&crc, hdr, sizeof(soter_container_hdr_t) - sizeof(uint32_t));
     soter_crc32_update(&crc, &dummy_crc, sizeof(uint32_t));
-    soter_crc32_update(&crc, hdr + 1, be32toh(hdr->size) - sizeof(soter_container_hdr_t));
+    soter_crc32_update(&crc, hdr + 1, soter_container_data_size(hdr));
 
     if (hdr->crc == htobe32(soter_crc32_final(&crc))) {
         return SOTER_SUCCESS;
@@ -45,20 +56,37 @@ soter_status_t soter_verify_container_checksum(const soter_container_hdr_t* hdr)
 
 uint8_t* soter_container_data(soter_container_hdr_t* hdr)
 {
+    if (!hdr) {
+        return NULL;
+    }
     return (uint8_t*)(hdr + 1);
 }
 
 const uint8_t* soter_container_const_data(const soter_container_hdr_t* hdr)
 {
+    if (!hdr) {
+        return NULL;
+    }
     return (const uint8_t*)(hdr + 1);
 }
 
 size_t soter_container_data_size(const soter_container_hdr_t* hdr)
 {
-    return be32toh(hdr->size) - sizeof(soter_container_hdr_t);
+    size_t size = 0;
+    if (!hdr) {
+        return 0;
+    }
+    size = be32toh(hdr->size);
+    if (size < sizeof(soter_container_hdr_t)) {
+        return 0;
+    }
+    return size - sizeof(soter_container_hdr_t);
 }
 
 void soter_container_set_data_size(soter_container_hdr_t* hdr, size_t size)
 {
+    if (!hdr || size > UINT32_MAX - sizeof(soter_container_hdr_t)) {
+        return;
+    }
     hdr->size = htobe32(size + sizeof(soter_container_hdr_t));
 }
