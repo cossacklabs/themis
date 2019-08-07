c475dc1bd18444d6227949f119865b054c6d78a3
vixentael <vixentael@users.noreply.github.com>
2019-08-07 14:17:50 +0300

    yet another php manual update (#519)

--- tests/phpthemis/composer-setup.sh
+++ tests/phpthemis/composer-setup.sh
@@ -3,10 +3,10 @@
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "echo 'Installed hash = '; echo hash_file('SHA384', 'composer-setup.php');"
 
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') 
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') 
   { echo 'Valid signature, continue installation'; } 
 else 
-  { echo 'ERROR: Invalid installer signature, probably installer was updated, fix in /tests/phpthemis/composer-setup.sh'; unlink('composer-setup.php'); exit(1); } 
+  { echo '\nERROR: Invalid installer signature, probably installer was updated, fix in /tests/phpthemis/composer-setup.sh'; unlink('composer-setup.php'); exit(1); } 
 echo PHP_EOL;"
 
 echo "If installation fails, means installer signature is invalid"
