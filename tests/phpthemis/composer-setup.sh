a3abce44fc9fdcf71d5dfa0be11723dd0843e227
Alexei Lozovsky <alexei@cossacklabs.com>
2019-12-12 11:37:05 +0200

    Update PHP Composer checksum (#566)
    Yes, they made a new release. We should probably revisit our pinning
    approach with PHP Composer...

--- tests/phpthemis/composer-setup.sh
+++ tests/phpthemis/composer-setup.sh
@@ -3,7 +3,7 @@
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "echo 'Installed hash = '; echo hash_file('SHA384', 'composer-setup.php');"
 
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') 
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '106d3d32cc30011325228b9272424c1941ad75207cb5244bee161e5f9906b0edf07ab2a733e8a1c945173eb9b1966197')
   { echo 'Valid signature, continue installation'; } 
 else 
   { echo '\nERROR: Invalid installer signature, probably installer was updated, fix in /tests/phpthemis/composer-setup.sh'; unlink('composer-setup.php'); exit(1); } 
