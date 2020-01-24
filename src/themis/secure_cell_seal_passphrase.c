bc62091e74cb1684abec8180dc176f8d8db81439
ilammy <alexei@cossacklabs.com>
2020-01-24 14:44:57 +0200

    Validate key length on decryption
    This is the first crash found by AFL. Decryption code fails to validate
    key length encoded in "algorithm" field of Secure Cell header and just
    proceeds to use it, assuming that it's one of 128, 192, 256 bits.
    
    Maximum value allowed by data format is 4096 bits (512 bytes), but the
    code allocates only 32 bytes for the derived key. Longer keys will cause
    a buffer overflow which allows an attacker to overwrite return address
    stored on the stack and to theoretically execute arbitrary code.
    Practically, this can be used for denial of service by exploiting the
    much more likely outcome -- crash due to a segmentation fault.
    
    Fix the issue by validating the key length and allowing only values
    produced by Themis: 128, 192, or 256 bits.

--- src/themis/secure_cell_seal_passphrase.c
+++ src/themis/secure_cell_seal_passphrase.c
@@ -297,6 +297,19 @@ static themis_status_t themis_auth_sym_derive_decryption_key(
 {
     size_t required_length = soter_alg_key_length(hdr->alg);
     /*
+     * Accept only expected values that we know that we can handle. Robustness
+     * is more important here than future-proofing. AES is unlikely to support
+     * more key lengths anyway.
+     */
+    switch (required_length) {
+    case SOTER_SYM_256_KEY_LENGTH / 8:
+    case SOTER_SYM_192_KEY_LENGTH / 8:
+    case SOTER_SYM_128_KEY_LENGTH / 8:
+        break;
+    default:
+        return THEMIS_FAIL;
+    }
+    /*
      * This is our internal buffer so it cannot be too small. If it is, either
      * the data is corrupted, or THEMIS_AUTH_SYM_MAX_KEY_LENGTH is incorrect.
      */
