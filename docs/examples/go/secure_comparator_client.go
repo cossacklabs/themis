0cf13ce4256d84ac5a3cc04c98fe98b419fc836c
ilammy <alexei@cossacklabs.com>
2020-02-16 00:08:37 +0200

    Action: GoThemis
    This is one of the most straighforward actions -- but not so fast!
    GoThemis build actually requires GOTHEMIS_IMPORT variable to be set,
    just be aware of that. "make test_go" relies on it.
    
    Take care to test against multiple versions of Go. We had issues with
    that in the past so this environment is important. Unfortunately,
    installing multiple versions of Go is not that easy with available
    actions. But I don't want to write a new one (or some shell scripts)
    so we simply run them in parallel.
    
    Also, touch up code examples: gofmt them and make sure they os.Exit(1)
    when then fail.

--- docs/examples/go/secure_comparator_client.go
+++ docs/examples/go/secure_comparator_client.go
@@ -2,50 +2,51 @@ package main
 
 import (
 	"fmt"
-	"github.com/cossacklabs/themis/gothemis/compare"
 	"net"
 	"os"
+
+	"github.com/cossacklabs/themis/gothemis/compare"
 )
 
 func main() {
 	if 2 != len(os.Args) {
 		fmt.Printf("usage: %s <password>", os.Args[0])
-		return
+		os.Exit(1)
 	}
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		fmt.Println("connection error")
-		return
+		os.Exit(1)
 	}
 	sc, err := compare.New()
 	if err != nil {
 		fmt.Println("error creating secure comparator object")
-		return
+		os.Exit(1)
 	}
 
 	err = sc.Append([]byte(os.Args[1]))
 	if err != nil {
 		fmt.Println("error appending secret to secure comparator")
-		return
+		os.Exit(1)
 	}
 
 	buf, err := sc.Begin()
 	if err != nil {
 		fmt.Println("error creating init message")
-		return
+		os.Exit(1)
 	}
 
 	for {
 		_, err = conn.Write(buf)
 		if err != nil {
 			fmt.Println("error writing bytes from socket")
-			return
+			os.Exit(1)
 		}
 
 		res, err := sc.Result()
 		if err != nil {
 			fmt.Println("error geting result from secure comparator")
-			return
+			os.Exit(1)
 		}
 
 		if compare.NotReady == res {
@@ -53,23 +54,22 @@ func main() {
 			readBytes, err := conn.Read(buf)
 			if err != nil {
 				fmt.Println("error reading bytes from socket")
-				return
+				os.Exit(1)
 			}
 			buffer, err := sc.Proceed(buf[:readBytes])
 			if nil != err {
 				fmt.Println("error unwraping message")
-				return
+				os.Exit(1)
 			}
 			buf = buffer
 		} else {
 			if compare.Match == res {
 				fmt.Println("match")
+				break
 			} else {
 				fmt.Println("not match")
+				os.Exit(1)
 			}
-			break
-
 		}
 	}
-
 }
