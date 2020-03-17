723a32e4266cb20faf4214a81ad2212b23efbacf
ilammy <alexei@cossacklabs.com>
2020-03-17 15:16:49 +0200

    Update Secure Cell API: Token Protect mode
    Token Protect mode is the one which gets most updates. Along with the
    same API renaming as other modes, there are two important changes:
    
    - decryption API now accepts message and token separately
    - encryption API returns a new helper object
    
    Previous decryption API accepts input via TSCellTokenEncryptedData
    objects -- the same ones as returned by encryption API. While this
    makes it easy to write unit tests, in real usage mesage and token
    are retrieved separately. Constructing an intermediate
    TSCellTokenEncryptedData object is completely unnecessary.
    New API rectifies this issue.
    
    Encryption API returns a new helper object TSCellTokenEncryptedResult
    which is almost idential to TSCellTokenEncryptedData, but uses NSData
    instead of NSMutableData. This allows for better Swift experience as
    it's directly bridged with Data, without any NSMutableData casts.
    
    TSCellTokenEncryptedResult also uses a different name for encrypted
    message, consistent with documentation and Android API. The old name
    is accessible to make it easier to port existing code that might
    operate on TSCellTokenEncryptedData instances.

--- src/wrappers/themis/Obj-C/objcthemis/scell_token.h
+++ src/wrappers/themis/Obj-C/objcthemis/scell_token.h
@@ -41,6 +41,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+/**
+ * Encryption result returned by Secure Cell in Token Protect mode.
+ */
+@interface TSCellTokenEncryptedResult : NSObject
+
+/** Encrypted data. */
+@property (nonatomic, readonly) NSData *encrypted;
+/** Authentication token. */
+@property (nonatomic, readonly) NSData *token;
+
+/**
+ * Encrypted data.
+ * @deprecated since Themis 0.13
+ * @see encrypted
+ */
+@property (nonatomic, readonly) NSData *cipherText
+    __deprecated_msg("use 'encrypted' instead");
+
+@end
 
 /** @brief Secure Cell Context Token Protect interface
 *
@@ -59,44 +78,168 @@ NS_ASSUME_NONNULL_BEGIN
 @interface TSCellToken : TSCell
 
 /**
-* @brief Initialize Secure cell object in context token mode
-* @param [in] key master key
-*/
+ * Initialise Secure Cell in Token Protect mode with a master key.
+ *
+ * @param [in] key  non-empty master key
+ *
+ * @returns @c nil if key is empty.
+ */
 - (nullable instancetype)initWithKey:(NSData *)key;
 
 /**
-* @brief Wrap message
-* @param [in] message message to wrap
-* @param [in] error pointer to Error on failure
-* @return Wrapped message as NSData object on success or nil on failure
-*/
-- (nullable TSCellTokenEncryptedData *)wrapData:(NSData *)message error:(NSError *__autoreleasing *)error;
+ * Encrypt data.
+ *
+ * @param [in]  message data to encrypt, must not be empty
+ * @param [in]  context optional associated context
+ * @param [out] error   optional error output
+ *
+ * Data is encrypted and authentication token is produced separately.
+ *
+ * The context, if provided, is cryptographically mixed with the data, but it not included
+ * into the resulting encrypted message. You will have to provide the same context again
+ * during decryption. Usually this is some plaintext data associated with encrypted data,
+ * such as database row number, protocol message ID, etc.
+ *
+ * @returns Successful encryption returns an object containing encrypted data and
+ * authentication token.
+ *
+ * @returns @c nil will be returned on failure, such as if @c message is empty
+ * or in case of some internal failure in cryptographic backend.
+ *
+ * @returns If @c error is not @c nil, it will describe the reason of failure.
+ */
+- (nullable TSCellTokenEncryptedResult *)encrypt:(NSData *)message
+                                         context:(nullable NSData *)context
+                                           error:(NSError **)error;
 
 /**
-* @brief Unwrap message
-* @param [in] message message to unwrap
-* @param [in] error pointer to Error on failure
-* @return Unwrapped message as NSData object on success or nil on failure
-*/
-- (nullable NSData *)unwrapData:(TSCellTokenEncryptedData *)message error:(NSError *__autoreleasing *)error;
+ * Encrypt data.
+ *
+ * @param [in]  message data to encrypt, must not be empty
+ * @param [out] error   optional error output
+ *
+ * Data is encrypted and authentication token is produced separately.
+ *
+ * This call is equivalent to encryption with @c nil associated context.
+ *
+ * @returns Successful encryption returns an object containing encrypted data and
+ * authentication token.
+ *
+ * @returns @c nil will be returned on failure, such as if @c message is empty
+ * or in case of some internal failure in cryptographic backend.
+ *
+ * @returns If @c error is not @c nil, it will describe the reason of failure.
+ */
+- (nullable TSCellTokenEncryptedResult *)encrypt:(NSData *)message error:(NSError **)error;
 
 /**
-* @brief Wrap message with context
-* @param [in] message message to wrap
-* @param [in] context user context
-* @param [in] error pointer to Error on failure
-* @return Wrapped message as NSData object on success or nil on failure
-*/
-- (nullable TSCellTokenEncryptedData *)wrapData:(NSData *)message context:(nullable NSData *)context error:(NSError *__autoreleasing *)error;
+ * Encrypt data.
+ * @see encrypt:context:error:
+ */
+- (nullable TSCellTokenEncryptedResult *)encrypt:(NSData *)message context:(nullable NSData *)context
+    NS_SWIFT_UNAVAILABLE("use throwing encrypt(_:,context:)");
 
 /**
-* @brief Unwrap message with context
-* @param [in] message message to unwrap
-* @param [in] context user context
-* @param [in] error pointer to Error on failure
-* @return Unwrapped message as NSData object on success or nil on failure
-*/
-- (nullable NSData *)unwrapData:(TSCellTokenEncryptedData *)message context:(nullable NSData *)context error:(NSError *__autoreleasing *)error;
+ * Encrypt data.
+ * @see encrypt:context:error:
+ */
+- (nullable TSCellTokenEncryptedResult *)encrypt:(NSData *)message
+    NS_SWIFT_UNAVAILABLE("use throwing encrypt");
+
+/**
+ * Encrypt data.
+ * @deprecated since Themis 0.13
+ * @see encrypt:context:error:
+ */
+- (nullable TSCellTokenEncryptedData *)wrapData:(NSData *)message context:(nullable NSData *)context error:(NSError **)error
+    __deprecated_msg("use 'encrypt:context:error:' instead");
+
+/**
+ * Encrypt data.
+ * @deprecated since Themis 0.13
+ * @see encrypt:error:
+ */
+- (nullable TSCellTokenEncryptedData *)wrapData:(NSData *)message error:(NSError **)error
+    __deprecated_msg("use 'encrypt:error:' instead");
+
+/**
+ * Decrypt data.
+ *
+ * @param [in]  message data to decrypt, cannot be empty
+ * @param [in]  token   authentication token to verify, cannot be empty
+ * @param [in]  context optional associated context
+ * @param [out] error   optional error output
+ *
+ * Secure Cell validates association with the context data, decrypts the message,
+ * and verifies data integrity using the provided authentication token.
+ *
+ * You need to provide the same context as it was used during encryption
+ * (or use @c nil or empty context if there was no context).
+ *
+ * @returns Decrypted data is returned if everything goes well.
+ *
+ * @returns @c nil will be returned on failure, such as if @c message or @c token
+ * is empty, or if the data has been tampered with, or the secret or associated context
+ * are not the same as were used to encrypt the data.
+ *
+ * @returns If @c error is not @c nil, it will describe the reason of failure.
+ */
+- (nullable NSData *)decrypt:(NSData *)message
+                       token:(NSData *)token
+                     context:(nullable NSData *)context
+                       error:(NSError **)error;
+
+/**
+ * Decrypt data.
+ *
+ * @param [in]  message data to decrypt, cannot be empty
+ * @param [in]  token   authentication token to verify, cannot be empty
+ * @param [out] error   optional error output
+ *
+ * Secure Cell decrypts the message and verifies data integrity using
+ * the provided authentication token.
+ *
+ * This call is equivalent to decryption with @c nil associated context.
+ *
+ * @returns Decrypted data is returned if everything goes well.
+ *
+ * @returns @c nil will be returned on failure, such as if @c message or @c token
+ * is empty, or if the data has been tampered with, or the secret or associated context
+ * are not the same as were used to encrypt the data.
+ *
+ * @returns If @c error is not @c nil, it will describe the reason of failure.
+ */
+- (nullable NSData *)decrypt:(NSData *)message token:(NSData *)token error:(NSError **)error;
+
+/**
+ * Decrypt data.
+ * @see decrypt:token:context:error:
+ */
+- (nullable NSData *)decrypt:(NSData *)message token:(NSData *)token context:(nullable NSData *)context
+    NS_SWIFT_UNAVAILABLE("use throwing decrypt(_:,token:,context:)");
+
+/**
+ * Decrypt data.
+ * @see decrypt:token:context:error:
+ */
+- (nullable NSData *)decrypt:(NSData *)message token:(NSData *)token
+    NS_SWIFT_UNAVAILABLE("use throwing decrypt(_:,context:)");
+
+/**
+ * Decrypt data.
+ * @deprecated since Themis 0.13
+ * @see decrypt:token:context:error:
+ */
+- (nullable NSData *)unwrapData:(TSCellTokenEncryptedData *)message context:(nullable NSData *)context error:(NSError **)error
+    __deprecated_msg("use 'decrypt:token:context:error:' instead");
+
+/**
+ * Decrypt data.
+ * @deprecated since Themis 0.13
+ * @see decrypt:token:error:
+ */
+- (nullable NSData *)unwrapData:(TSCellTokenEncryptedData *)message error:(NSError **)error
+    __deprecated_msg("use 'decrypt:token:error:' instead");
 
 @end
 
