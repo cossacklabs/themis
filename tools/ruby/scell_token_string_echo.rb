83f25359a300aa7b9e91b75d8f35b5304c2e659d
Alexei Lozovsky <alexei@cossacklabs.com>
2020-03-18 14:42:06 +0200

    Secure Cell passphrase API: RbThemis (#603)
    * Store error code in ThemisError
    
    Add "error_code" attribute that keeps the original error code returned
    by Themis Core. This should make debugging issues a bit easier. Use
    "invalid argument" as default value since that's the most common issue.
    
    * Use new Secure Cell API in tests
    
    First of all, update the tests to use new API and correctly distinguish
    between passphrases and master keys. Add some new tests to verify
    message integrity. Additionally, add compatibility tests which verify
    that old API is still supported.
    
    * New Secure Cell API
    
    Add new classes implementing updated Secure Cell API and adding
    passphrase support for Seal mode:
    
      - ScellSeal
      - ScellSealPassphrase
      - ScellTokenProtect
      - ScellContextImprint
    
    This supercedes existing "Scell" class which is more or less deprecated.
    
    The new implementation checks key validity before encryption/decryption
    is attempted so new() methods may raise exceptions now.
    
    Token Protect API is now more reasonable and accepts encrypted message
    and authentication token as separate arguments for decryption. However,
    for compatibility with old API we accept them as a list as well.
    
    New passphrase API can be accessed via ScellSealPassphrase class.
    
    * Deprecate old Secure Cell API
    
    Since new Secure Cell API is compatible with the old one (except for
    initialization) we can remove all those swathes of code and replace
    them with new implementation.
    
    However, there are caveats due to how inheritance works in Ruby.
    
    Here we proxy encrypt/decrypt calls to the actual object. Alternative
    implementation could have looked like
    
        class Scell
          # Override "new" instead of "initialize" since that allows to
          # return a different instance, possibly even unrelated one.
          def self.new(key, mode)
            case mode
            when SEAL_MODE
              return ScellSeal.new(key)
            # ...
            end
          end
        end
    
    This approach is a bit more efficient because there is no indirection in
    encrypt/decrypt calls. However, this approach also makes it impossible
    for ScellSeal to inherit from Scell. We would like to keep the
    inheritance chain, just in case some code relies on the fact that all
    Secure Cells are "kind_of? Themis::Scell". Hence... this.
    
    Compatibility notwithstanding, Themis::Scell is considered deprecated
    from now on. Since we have compatibility tests, make sure these warnings
    are suppressed so that we are not annoyed by them in test output.
    
    * Use new Secure Cell API in integration tools
    
    Update existing tools to use new API and add a new tool to verify
    passphrase API across other languages.
    
    * Use new Secure Cell API in code samples
    
    Update code example to use new API. It's kinda dated and terse, bring it
    to the same informational level as other languages. Also, fix shebang to
    make the example executable directly.
    
    P.S. Those historical names are amusing: full, auto split, user split...
    
    * Make string_to_pointer_size pure
    
    This function is used to convert a string into an FFI::MemoryPointer
    with string's bytes that we can pass to Themis Core. It's current
    implementation is... not ideal because it uses "force_encoding".
    
    This is problematic because it does not return a copy of a string with
    different encoding. It modifies the encoding of the string in-place.
    This is unexpected for client code *and* does not work for fronzen
    strings (e.g., literal constants).
    
    Update the implementation to use FFI::MemoryPointer.from_string which
    has the same behavior (returns a pointer into string's buffer) but
    does not change the encoding of the string.
    
    This function is used throughout RbThemis code, not only by Secure Cell.
    It should be enough to add encoding tests for Secure Cell only, for now.

--- tools/ruby/scell_token_string_echo.rb
+++ tools/ruby/scell_token_string_echo.rb
@@ -36,7 +36,7 @@ if input_args.length == 4
 	context = input_args[3].dup
 end
 
-scell = Themis::Scell.new(key, Themis::Scell::TOKEN_PROTECT_MODE)
+scell = Themis::ScellTokenProtect.new(key)
 
 if command == "enc"
 	encr_message, token = scell.encrypt(message, context)
