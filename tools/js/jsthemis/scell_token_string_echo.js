68c416b3490097eeba419dd8ed707c51f585d0df
ilammy <me@ilammy.net>
2020-04-09 14:57:28 +0300

    Use new API in integration tools
    Update integration testing tools to use new Secure Cell API (withKey),
    and generally unify the tools with WasmThemis.

--- tools/js/jsthemis/scell_token_string_echo.js
+++ tools/js/jsthemis/scell_token_string_echo.js
@@ -1,41 +1,43 @@
-var jsthemis = require('jsthemis');
+const themis = require('jsthemis')
 
-if (process.argv.length < 5 || process.argv.length > 6) {
-    console.log('Usage: <command: enc | dec > <key> <message> <context (optional)>');
+let command, key, message, context
+if (5 <= process.argv.length && process.argv.length <= 6) {
+    command = process.argv[2]
+    key     = process.argv[3]
+    message = process.argv[4]
+    if (process.argv.length == 6) {
+        context = process.argv[5]
+    }
+} else {
+    console.log('usage: node scell_token_string_echo.js {enc|dec} <key> <message[,token]> [<context>]')
     process.exit(1);
 }
 
-var command = process.argv[2];
-var key = new Buffer(process.argv[3]);
-var message = process.argv[4];
-var context = undefined;
-if(process.argv.length === 6){
-    var context = new Buffer(process.argv[5]);
-}
-
-if (command === "enc"){
-    var message_buf = new Buffer(message);
-    var encrypter = new jsthemis.SecureCellTokenProtect(key);
-    if (context){
-        var result = encrypter.encrypt(message_buf, context);
-    } else {
-        var result = encrypter.encrypt(message_buf);
-    }
-    console.log([result.data.toString('base64'), result.token.toString('base64')].join(','));
-    process.exit(0)
-} else if (command === 'dec'){
-    var splitted_message = message.split(',');
-    var decrypter = new jsthemis.SecureCellTokenProtect(key);
-    var decoded_message = new Buffer(splitted_message[0], 'base64');
-    var decoded_token = new Buffer(splitted_message[1], 'base64');
-    if(context){
-        var result = decrypter.decrypt(decoded_message, decoded_token, context);
-    } else {
-        var result = decrypter.decrypt(decoded_message, decoded_token);
-    }
-    console.log(result.toString('ascii'));
-    process.exit(0)
-} else {
-    console.log('Wrong command, use "enc" or "dec"');
-    process.exit(1)
+let cell = themis.SecureCellTokenProtect.withKey(Buffer.from(key))
+let result
+switch (command) {
+    case 'enc':
+        if (context) {
+            result = cell.encrypt(Buffer.from(message), Buffer.from(context))
+        } else {
+            result = cell.encrypt(Buffer.from(message))
+        }
+        console.log(Buffer.from(result.data).toString('base64') + ','
+                    + Buffer.from(result.token).toString('base64'))
+        break
+    case 'dec':
+        let [the_message, token] = message.split(',')
+        if (context) {
+            result = cell.decrypt(Buffer.from(the_message, 'base64'),
+                                  Buffer.from(token, 'base64'),
+                                  Buffer.from(context))
+        } else {
+            result = cell.decrypt(Buffer.from(the_message, 'base64'),
+                                  Buffer.from(token, 'base64'))
+        }
+        console.log(Buffer.from(result).toString('utf-8'))
+        break
+    default:
+        console.log('invalid command "' + command + '": use "enc" or "dec"')
+        process.exit(1)
 }
