11ea972c4a77f65c86f9923137ea0f084b78e207
Alexei Lozovsky <alexei@cossacklabs.com>
2020-04-22 17:56:06 +0300

    Improve GoThemis error introspection (#622)
    Add numeric error code to ThemisError struct, accessible via "Code()"
    method. Now it will be possible to communicate Themis Core errors to
    the user code. This is intended for programmatic consumption. Human-
    readable descriptive message is still available via Error().
    
    Errors with code can be constructed using "NewWithCode()" constructor.
    Existing code sites have been updated to use it for reporting various
    early warnings about invalid arguments. Themis Core checks for these
    violations too, we do these checks in Go to be able to provide more
    specific descriptions to users.
    
    Note that there are still uses of errors.New(). Not all-- I mean, *all*
    CGo call sites right now do not return Themis Core errors. This will be
    refactored eventually. For now, these call sites with use a placeholder
    THEMIS_FAIL as the code, which is going to be correct in 99% cases.
    
    Also note that we now export error code constants. All of this makes it
    easier for user code to decide on an error type. It will also make it
    easier for us to write new CGo code as we can keep use of C to minimum.

--- gothemis/errors/error.go
+++ gothemis/errors/error.go
@@ -1,18 +1,71 @@
+/*
+ * Copyright (c) 2016 Cossack Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package errors describes errors returned by GoThemis functions.
 package errors
 
-// ThemisError is an operational error.
+/*
+#include <themis/themis.h>
+*/
+import "C"
+
+// ThemisErrorCode describes an error reported by Themis Core.
+type ThemisErrorCode int
+
+// Error code constants.
+//
+// These are numeric error codes used by Themis Core (see `<themis/themis_error.h>`).
+const (
+	Success          ThemisErrorCode = C.THEMIS_SUCCESS
+	Fail                             = C.THEMIS_FAIL
+	InvalidParameter                 = C.THEMIS_INVALID_PARAMETER
+	NoMemory                         = C.THEMIS_NO_MEMORY
+	BufferTooSmall                   = C.THEMIS_BUFFER_TOO_SMALL
+	DataCorrupt                      = C.THEMIS_DATA_CORRUPT
+	InvalidSignature                 = C.THEMIS_INVALID_SIGNATURE
+	NotSupported                     = C.THEMIS_NOT_SUPPORTED
+)
+
+// ThemisError is a common type of GoThemis errors.
 type ThemisError struct {
-	msg string
+	description string
+	errorCode   ThemisErrorCode
 }
 
-// Error returns textual description of the error.
+// Error returns human-readable textual description of the error.
 func (e *ThemisError) Error() string {
-	return e.msg
+	return e.description
+}
+
+// Code returns machine-readable numeric error code.
+func (e *ThemisError) Code() ThemisErrorCode {
+	return e.errorCode
 }
 
 // New makes an error with provided description.
-func New(msg string) *ThemisError {
-	return &ThemisError{msg}
+func New(description string) *ThemisError {
+	// This constructor is used by default in legacy CGo code which does not return
+	// accurate Themis Core error codes. Use "Fail" which is likely to be correct
+	// and is a good generic placeholder for an error.
+	return NewWithCode(Fail, description)
+}
+
+// NewWithCode makes an error with provided numeric code and description.
+func NewWithCode(code ThemisErrorCode, description string) *ThemisError {
+	return &ThemisError{description, code}
 }
 
 // ThemisCallbackError is user-generated error returned from Secure Session callback.
