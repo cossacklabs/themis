fc6d8c44160805b4717d68c76b77e4900b23a6dc
Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>
2020-08-18 16:13:02 +0300

    Add C++ tests

--- tests/themispp/secure_session_test.hpp
+++ tests/themispp/secure_session_test.hpp
@@ -27,6 +27,7 @@
 
 #include <themispp/secure_session.hpp>
 
+#include "secure_session_test_keys.hpp"
 #include "utils.hpp"
 
 // Allow usage of non-owning Secure Session constructor for testing
@@ -74,37 +75,7 @@ namespace secure_session_test
 static const std::vector<uint8_t> client_id = as_bytes("client");
 static const std::vector<uint8_t> server_id = as_bytes("server");
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays): C++03 compatbility
-const uint8_t server_priv[] = {0x52, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0x6c,
-                               0x07, 0x99, 0x10, 0x00, 0x49, 0xbe, 0x76, 0x15, 0x5e,
-                               0xeb, 0x8b, 0x68, 0xb5, 0x7f, 0x0f, 0xc4, 0xfd, 0xb5,
-                               0x16, 0x41, 0x74, 0x7c, 0xf4, 0xe3, 0xed, 0xd7, 0xaf,
-                               0x02, 0x7e, 0x95, 0x7d, 0xcf, 0x6d, 0x9b, 0x4e, 0x4a};
-static const std::vector<uint8_t> server_private_key = as_bytes(server_priv);
-
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays): C++03 compatbility
-const uint8_t server_pub[] = {0x55, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0x16,
-                              0x2d, 0x68, 0x68, 0x03, 0x96, 0x1a, 0xcd, 0xd1, 0x5b,
-                              0x01, 0xea, 0x18, 0xb1, 0x63, 0x83, 0x2b, 0xe6, 0x85,
-                              0x8d, 0x5e, 0xbf, 0xd2, 0xb0, 0xc2, 0xe1, 0x2a, 0xa3,
-                              0x24, 0xeb, 0x37, 0xd3, 0xe7, 0xc7, 0xa2, 0xab, 0xdd};
-static const std::vector<uint8_t> server_public_key = as_bytes(server_pub);
-
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays): C++03 compatbility
-const uint8_t client_priv[] = {0x52, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0xf2,
-                               0x6a, 0x06, 0x8c, 0x00, 0x5b, 0xd7, 0x35, 0x5c, 0x53,
-                               0x41, 0x89, 0xcd, 0x08, 0xa2, 0x30, 0xd2, 0x0b, 0x32,
-                               0x4c, 0x74, 0x87, 0xc9, 0x0c, 0x2e, 0x9c, 0x86, 0xbf,
-                               0x40, 0xf6, 0xea, 0xac, 0x8a, 0x75, 0x63, 0x67, 0xbc};
-static const std::vector<uint8_t> client_private_key = as_bytes(client_priv);
-
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays): C++03 compatbility
-const uint8_t client_pub[] = {0x55, 0x45, 0x43, 0x32, 0x00, 0x00, 0x00, 0x2d, 0xba,
-                              0x94, 0x97, 0xf7, 0x03, 0x06, 0x4f, 0x4c, 0xe0, 0x82,
-                              0x74, 0xe3, 0xe9, 0x23, 0xa3, 0xac, 0x57, 0x1f, 0x1e,
-                              0xf8, 0xf0, 0x66, 0x85, 0xee, 0xa3, 0x4c, 0x4f, 0xd5,
-                              0xb9, 0x85, 0xc2, 0xde, 0x39, 0x49, 0x8d, 0x99, 0xe1};
-static const std::vector<uint8_t> client_public_key = as_bytes(client_pub);
+/* Test keys can be found in secure_session_test_keys.hpp */
 
 class callback : public themispp::secure_session_callback_interface_t
 {
@@ -261,6 +232,42 @@ static void secure_session_ownership()
 }
 #endif
 
+static void secure_session_with_rsa_key()
+{
+    bool init_throws = false;
+    try {
+        callback client_callbacks;
+        themispp::secure_session_t client(client_id, client_private_rsa_key, &client_callbacks);
+    } catch (const themispp::exception_t&) {
+        init_throws = true;
+    }
+    sput_fail_unless(init_throws, "using RSA private key", __LINE__);
+}
+
+static void secure_session_with_ec_pub_key()
+{
+    bool init_throws = false;
+    try {
+        callback client_callbacks;
+        themispp::secure_session_t client(client_id, client_public_key, &client_callbacks);
+    } catch (const themispp::exception_t&) {
+        init_throws = true;
+    }
+    sput_fail_unless(init_throws, "using EC public key", __LINE__);
+}
+
+static void secure_session_with_rsa_pub_key()
+{
+    bool init_throws = false;
+    try {
+        callback client_callbacks;
+        themispp::secure_session_t client(client_id, client_public_rsa_key, &client_callbacks);
+    } catch (const themispp::exception_t&) {
+        init_throws = true;
+    }
+    sput_fail_unless(init_throws, "using RSA public key", __LINE__);
+}
+
 inline int run_secure_session_test()
 {
     sput_enter_suite("ThemisPP secure session test");
@@ -269,6 +276,9 @@ inline int run_secure_session_test()
     sput_run_test(secure_session_uninitialized, "secure_session_uninitialized", __FILE__);
     sput_run_test(secure_session_moved, "secure_session_moved", __FILE__);
     sput_run_test(secure_session_ownership, "secure_session_ownership", __FILE__);
+    sput_run_test(secure_session_with_rsa_key, "secure_session_with_rsa_key", __FILE__);
+    sput_run_test(secure_session_with_ec_pub_key, "secure_session_with_ec_pub_key", __FILE__);
+    sput_run_test(secure_session_with_rsa_pub_key, "secure_session_with_rsa_pub_key", __FILE__);
     return 0;
 }
 
