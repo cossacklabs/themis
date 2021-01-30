27a67fd376a7e95f5869b58945235cd45bfdbfa0
Alexei Lozovsky <me@ilammy.net>
2021-01-30 14:36:18 +0900

    Update doc server links in JavaThemis comments

--- src/wrappers/themis/java/com/cossacklabs/themis/SecureCell.java
+++ src/wrappers/themis/java/com/cossacklabs/themis/SecureCell.java
@@ -103,13 +103,13 @@ import java.nio.charset.StandardCharsets;
  * For example, {@link SecureCell#SealWithKey(SymmetricKey)} constructs Secure Cell in Seal mode
  * using a symmetric key.
  * <p>
- * <a href="https://docs.cossacklabs.com/pages/secure-cell-cryptosystem/">Here you can learn more</a>
+ * <a href="https://docs.cossacklabs.com/themis/crypto-theory/cryptosystems/secure-cell/">Here you can learn more</a>
  * about the underlying considerations, limitations, and features of each mode.
  *
  * @since JavaThemis 0.9
  */
 public class SecureCell {
-	
+
 	static {
 		System.loadLibrary("themis_jni");
 	}
@@ -142,7 +142,7 @@ public class SecureCell {
      * use passphrase API instead: {@link SecureCell#SealWithPassphrase(String)}.
      * <p>
      * You can read more about Seal mode
-     * <a href="https://docs.cossacklabs.com/pages/secure-cell-cryptosystem/#seal-mode">in documentation</a>.
+     * <a href="https://docs.cossacklabs.com/themis/crypto-theory/cryptosystems/secure-cell/#seal-mode">in documentation</a>.
      *
      * @since JavaThemis 0.13
      */
@@ -358,7 +358,7 @@ public class SecureCell {
      * then use {@link SecureCell#TokenProtectWithKey(SymmetricKey)} to construct a Secure Cell.
      * <p>
      * You can read more about Token Protect mode
-     * <a href="https://docs.cossacklabs.com/pages/secure-cell-cryptosystem/#token-protect-mode">in documentation</a>.
+     * <a href="https://docs.cossacklabs.com/themis/crypto-theory/cryptosystems/secure-cell/#token-protect-mode">in documentation</a>.
      *
      * @since JavaThemis 0.13
      */
@@ -512,7 +512,7 @@ public class SecureCell {
      * supply a different associated context for each encryption invocation with the same key.
      * <p>
      * You can read more about Context Imprint mode
-     * <a href="https://docs.cossacklabs.com/pages/secure-cell-cryptosystem/#context-imprint-mode">in documentation</a>.
+     * <a href="https://docs.cossacklabs.com/themis/crypto-theory/cryptosystems/secure-cell/#context-imprint-mode">in documentation</a>.
      *
      * @since JavaThemis 0.13
      */
@@ -619,10 +619,10 @@ public class SecureCell {
 		if (mode < MODE_SEAL || mode > MODE_CONTEXT_IMPRINT) {
 			throw new InvalidArgumentException("invalid mode");
 		}
-		
+
 		this.mode = mode;
 	}
-	
+
 	/**
 	 * Creates new SecureCell with default master key in SEAL mode
 	 * @param default master key
@@ -634,7 +634,7 @@ public class SecureCell {
 	public SecureCell(byte[] key) {
 		this.key = key;
 	}
-	
+
 	/**
 	 * Creates new SecureCell with default master key in specified mode
 	 * @param default master key
@@ -654,7 +654,7 @@ public class SecureCell {
 		this(mode);
 		this.key = key;
 	}
-	
+
 	/**
 	 * Creates new SecureCell with default master password in SEAL mode
 	 * @param default master password
@@ -673,7 +673,7 @@ public class SecureCell {
 	public SecureCell(String password) {
 		this(password.getBytes(CHARSET));
 	}
-	
+
 	/**
 	 * Creates new SecureCell with default master password in specified mode
 	 * @param default master password
@@ -700,10 +700,10 @@ public class SecureCell {
 		this(mode);
 		this.key = password.getBytes(CHARSET);
 	}
-	
+
 	int mode = MODE_SEAL;
 	byte[] key;
-	
+
 	static final Charset CHARSET = StandardCharsets.UTF_16;
 
 	/** @deprecated since JavaThemis 0.13 */
@@ -716,7 +716,7 @@ public class SecureCell {
 	@Deprecated
 	public static final int MODE_CONTEXT_IMPRINT = 2;
 	static final int MODE_SEAL_PASSPHRASE = 3;
-	
+
 	static native byte[][] encrypt(byte[] key, byte[] context, byte[] data, int mode);
 	static native byte[] decrypt(byte[] key, byte[] context, byte[][] protectedData, int mode);
 
@@ -725,24 +725,24 @@ public class SecureCell {
 		if (null == key) {
 			throw new NullArgumentException("Master key was not provided");
 		}
-		
+
 		if (null == data) {
 			throw new NullArgumentException("Data was not provided");
 		}
-		
+
 		if (MODE_CONTEXT_IMPRINT == mode) {
 			// Context is mandatory for this mode
 			if (null == context) {
 				throw new NullArgumentException("Context is mandatory for context imprint mode");
 			}
 		}
-		
+
 		byte[][] protectedData = encrypt(key, context, data, mode);
-		
+
 		if (null == protectedData) {
 			throw new SecureCellException();
 		}
-		
+
 		return new SecureCellData(protectedData[0], protectedData[1]);
 	}
 
@@ -751,36 +751,36 @@ public class SecureCell {
 		if (null == key) {
 			throw new NullArgumentException("Master key was not provided");
 		}
-		
+
 		if (null == protectedData) {
 			throw new NullArgumentException("Protected data was not provided");
 		}
-		
+
 		if (MODE_CONTEXT_IMPRINT == mode) {
 			// Context is mandatory for this mode
 			if (null == context) {
 				throw new NullArgumentException("Context is mandatory for context imprint mode");
 			}
 		}
-		
+
 		if (null == protectedData.getProtectedData()) {
 			throw new InvalidArgumentException("protectedData");
 		}
-		
+
 		if (MODE_TOKEN_PROTECT == mode) {
 			if (null == protectedData.getAdditionalData()) {
 				throw new InvalidArgumentException("additionalData");
 			}
 		}
-		
+
 		byte[] data = decrypt(key, context, new byte[][]{protectedData.getProtectedData(), protectedData.getAdditionalData()}, mode);
 		if (null == data) {
 			throw new SecureCellException();
 		}
-		
+
 		return data;
 	}
-	
+
 	/**
 	 * Protects data with specified master key
 	 * @param master key to use for protecting data
@@ -797,7 +797,7 @@ public class SecureCell {
 	public SecureCellData protect(byte[] key, byte[] context, byte[] data) throws SecureCellException {
 		return protect(key, context, data, this.mode);
 	}
-	
+
 	/**
 	 * Protects data with default master key
 	 * @param context to which protected data will be bound (may be null)
@@ -813,7 +813,7 @@ public class SecureCell {
 	public SecureCellData protect(byte[] context, byte[] data) throws SecureCellException {
 		return this.protect(this.key, context, data);
 	}
-	
+
 	/**
 	 * Protects data with specified master password
 	 * @param master password to use for protecting data
@@ -833,7 +833,7 @@ public class SecureCell {
 	public SecureCellData protect(String password, String context, byte[] data) throws SecureCellException {
 		return this.protect(password.getBytes(CHARSET), context.getBytes(CHARSET), data);
 	}
-	
+
 	/**
 	 * Protects data with default master password
 	 * @param context to which protected data will be bound (may be null)
@@ -849,7 +849,7 @@ public class SecureCell {
 	public SecureCellData protect(String context, byte[] data) throws SecureCellException {
 		return this.protect(this.key, context.getBytes(CHARSET), data);
 	}
-	
+
 	/**
 	 * Decrypts and verifies protected data
 	 * @param master key
@@ -867,7 +867,7 @@ public class SecureCell {
 	public byte[] unprotect(byte[] key, byte[] context, SecureCellData protectedData) throws SecureCellException {
 		return unprotect(key, context, protectedData, this.mode);
 	}
-	
+
 	/**
 	 * Decrypts and verifies protected data with default master key
 	 * @param context to which protected data will is bound (may be null, must be same as provided in protect call)
@@ -884,7 +884,7 @@ public class SecureCell {
 	public byte[] unprotect(byte[] context, SecureCellData protectedData) throws SecureCellException {
 		return this.unprotect(this.key, context, protectedData);
 	}
-	
+
 	/**
 	 * Decrypts and verifies protected data
 	 * @param master password
@@ -905,7 +905,7 @@ public class SecureCell {
 	public byte[] unprotect(String password, String context, SecureCellData protectedData) throws SecureCellException {
 		return this.unprotect(password.getBytes(CHARSET), context.getBytes(CHARSET), protectedData);
 	}
-	
+
 	/**
 	 * Decrypts and verifies protected data with default master password
 	 * @param context to which protected data will is bound (may be null, must be same as provided in protect call)
