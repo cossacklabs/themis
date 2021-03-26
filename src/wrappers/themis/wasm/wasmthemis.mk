e5cc867cd5a767cb15723bf9bdca54018bebb0d2
Maximilian Ammann <max@maxammann.org>
2021-03-26 13:36:03 +0100

    Modularize wasm themis

--- src/wrappers/themis/wasm/wasmthemis.mk
+++ src/wrappers/themis/wasm/wasmthemis.mk
@@ -26,6 +26,7 @@ WASM_PACKAGE = $(BIN_PATH)/wasm-themis.tgz
 
 $(BIN_PATH)/libthemis.js: LDFLAGS += -s EXTRA_EXPORTED_RUNTIME_METHODS=@$(WASM_RUNTIME)
 $(BIN_PATH)/libthemis.js: LDFLAGS += -s ALLOW_TABLE_GROWTH
+$(BIN_PATH)/libthemis.js: LDFLAGS += -s MODULARIZE=1
 # FIXME(ilammy, 2020-11-29): rely in EMSCRIPTEN_KEEPALIVE instead of LINKABLE
 # For some reason existing EMSCRIPTEN_KEEPALIVE macros do not work and without
 # LINKABLE flag wasm-ld ends up stripping *all* Themis functions from "*.wasm"
