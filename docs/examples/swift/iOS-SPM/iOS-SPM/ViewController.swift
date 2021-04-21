42d29fd98c9957c074d78c7dfb296c5e5aa7e745
julepka <julepka@gmail.com>
2021-04-21 18:19:43 +0300

    add swift SPM test project

---
+++ docs/examples/swift/iOS-SPM/iOS-SPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-SPM
+//
+//  Created by Julia Potapenko on 21.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
