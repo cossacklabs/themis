b8e024faeca0baa9e6eab23c050ee3e6e65f9574
julepka <julepka@gmail.com>
2021-05-24 18:01:45 +0300

    Fixing Carthage examples embed settings (#827)

--- docs/examples/swift/iOS-Carthage/ThemisTest/AppDelegate.swift
+++ docs/examples/swift/iOS-Carthage/ThemisTest/AppDelegate.swift
@@ -17,6 +17,8 @@ import themis
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //
