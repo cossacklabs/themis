5c5c765b4963b577c6334af11725ae7e1203b202
Alexei Lozovsky <me@ilammy.net>
2021-06-28 23:57:33 +0900

    Example WasmThemis project with webpack
    Finally my dream is coming true: interactive example of Themis right in
    the browser. It's a bit sad that I have to work for my own dreams, but
    doing what I can with what I got.
    
    The first commit adds a skeleton with webpack 5, pulling in WasmThemis
    and half of the internet of other dependencies. Don't think about it too
    much, okay? This is normal for JavaScript.
    
    Right now we don't do anything, but at least you can see that WasmThemis
    has loaded successfully. Or not.

---
+++ docs/examples/wasm/webpack/README.md
@@ -0,0 +1,15 @@
+# WasmThemis Example
+
+This is an example of using WasmThemis in browser with **webpack**.
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Build and open in your browser:
+
+```
+npm run start
+```
---
+++ docs/examples/wasm/webpack/src/index.js
@@ -0,0 +1,10 @@
+// FIXME: use 'wasm-themis' instead of '@ilammy/wasm-themis-preview'
+import themis from '@ilammy/wasm-themis-preview'
+
+window.onload = function() {
+    themis.initialized.then(function() {
+        const loaded = document.getElementById('wasm-loaded')
+        loaded.textContent = 'WasmThemis loaded!'
+        loaded.classList.add('dimmed')
+    })
+}
