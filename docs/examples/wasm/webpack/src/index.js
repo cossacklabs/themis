83bbf407a93de20a3dbf0f6a8a366f8bec3222dc
Alexei Lozovsky <me@ilammy.net>
2021-06-30 21:17:06 +0900

    Secure Cell interactive example
    Throw in layout, code, and basic styling for interactive example of
    Secure Cell. You can input things and observe results. Performance
    is okayish for symmetric keys. Passphrases are slow. Damn slow.
    
    I thought about splitting this into commits, but no one reads these
    messages, right? 🍌 <BANANA MODIFIER VIOLET> So why bother?

--- docs/examples/wasm/webpack/src/index.js
+++ docs/examples/wasm/webpack/src/index.js
@@ -1,10 +1,12 @@
 // FIXME: use 'wasm-themis' instead of '@ilammy/wasm-themis-preview'
 import themis from '@ilammy/wasm-themis-preview'
+import { setupSecureCell } from './secure-cell.js'
 
 window.onload = function() {
     themis.initialized.then(function() {
         const loaded = document.getElementById('wasm-loaded')
         loaded.textContent = 'WasmThemis loaded!'
         loaded.classList.add('dimmed')
+        setupSecureCell()
     })
 }
