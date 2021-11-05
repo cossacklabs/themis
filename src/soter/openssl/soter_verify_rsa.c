75eed90176671fe97681827925358efbfa834af0
Alexei Lozovsky <me@ilammy.net>
2021-11-05 20:01:43 +0900

    verify/rsa: Use EVP_PKEY directly

--- src/soter/openssl/soter_verify_rsa.c
+++ src/soter/openssl/soter_verify_rsa.c
@@ -108,7 +108,7 @@ soter_status_t soter_verify_final_rsa_pss_pkcs8(soter_sign_ctx_t* ctx,
     if (!ctx) {
         return SOTER_INVALID_PARAMETER;
     }
-    EVP_PKEY* pkey = EVP_PKEY_CTX_get0_pkey(ctx->pkey_ctx);
+    EVP_PKEY* pkey = ctx->pkey;
     if (!pkey) {
         return SOTER_INVALID_PARAMETER;
     }
