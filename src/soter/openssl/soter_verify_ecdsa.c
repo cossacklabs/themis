8d78f4a57d76bf8a2e3dadb3d5485764efd4cbb3
Alexei Lozovsky <me@ilammy.net>
2021-11-06 20:06:17 +0900

    verify/ec: Check other arguments for NULL/zero too
    As a couresy, mostly. OpenSSL will still fail with such values, but
    we can return SOTER_INVALID_PARAMETER instead. (Though, I belive that
    all upstream callers of these functions already check for these. If so,
    the compiler would be free to optimize those checks during inlining.)

--- src/soter/openssl/soter_verify_ecdsa.c
+++ src/soter/openssl/soter_verify_ecdsa.c
@@ -89,6 +89,10 @@ soter_status_t soter_verify_update_ecdsa_none_pkcs8(soter_sign_ctx_t* ctx,
     if (!ctx) {
         return SOTER_INVALID_PARAMETER;
     }
+    if (!data || data_length == 0) {
+        return SOTER_INVALID_PARAMETER;
+    }
+
     if (!EVP_DigestVerifyUpdate(ctx->md_ctx, data, data_length)) {
         return SOTER_FAIL;
     }
@@ -101,6 +105,9 @@ soter_status_t soter_verify_final_ecdsa_none_pkcs8(soter_sign_ctx_t* ctx,
                                                    const size_t signature_length)
 {
     if (!ctx || !ctx->pkey) {
+        return SOTER_INVALID_PARAMETER;
+    }
+    if (!signature || signature_length == 0) {
         return SOTER_INVALID_PARAMETER;
     }
 
