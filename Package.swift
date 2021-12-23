d05da5bd6b1d1a3243bdfa6df3ef2bdea409fb0d
vixentael <vixentael@users.noreply.github.com>
2021-12-23 15:20:07 +0200

    Update Themis SPM package, iOS/macOS examples for 0.14.0 (#889)

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
     targets: [
         .binaryTarget(name: "themis",
                       // update version in URL path
-                      url: "https://github.com/cossacklabs/themis/releases/download/0.13.12/themis.xcframework.zip",
+                      url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
                       // The scripts/create_xcframework.sh calculates the checksum when generating the XCF.
                       // Alternatively, run from package directory:
                       // swift package compute-checksum build/xcf_output/themis.xcframework.zip
-                      checksum: "c74f65d4918884220efe99c3195001fa8aabc8030ad85f8ef30d2bfed11065a3"),
+                      checksum: "182587c9070b94e552816d3cc174d64ea0f0a64d5b19483210bcf3dcac4d2658"),
 
     ]
 )
