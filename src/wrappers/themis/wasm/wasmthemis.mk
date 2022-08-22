ee7df68792d3aa775e9da05ab1eca69dff52ef8c
Alex Radetsky <alexey.radetsky@gmail.com>
2022-08-22 20:55:47 +0300

    hotfix wasm themis: rebuild wasm-themis with -O3 flag (#938)
    * rebuild wasm-themis with -O3 flag
    
    * update CHANGELOG.md

--- src/wrappers/themis/wasm/wasmthemis.mk
+++ src/wrappers/themis/wasm/wasmthemis.mk
@@ -39,9 +39,7 @@ $(BIN_PATH)/libthemis.js: LDFLAGS += -s LINKABLE=1
 # does not use are missing the from the binary. Suppress the warnings.
 $(BIN_PATH)/libthemis.js: LDFLAGS += -s ERROR_ON_UNDEFINED_SYMBOLS=0
 $(BIN_PATH)/libthemis.js: LDFLAGS += --pre-js $(WASM_PRE_JS)
-
 $(BIN_PATH)/libthemis.js: CMD = $(CC) -o $@ $(filter %.o %a, $^) -O3 $(LDFLAGS)
-
 $(BIN_PATH)/libthemis.js: $(THEMIS_STATIC) $(WASM_RUNTIME) $(WASM_PRE_JS)
 	@mkdir -p $(@D)
 	@echo -n "link "
