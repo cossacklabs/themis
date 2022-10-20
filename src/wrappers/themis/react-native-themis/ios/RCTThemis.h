04ba4b949e49a5df5c2ad2d9e04ba1f89ddea845
Alex Radetsky <alexey.radetsky@gmail.com>
2022-10-20 15:41:29 +0300

    Redesigned the framework to make it less vulnerable to native exceptions. (#955)
    * redesigned the framework to make it less vulnerable to native exceptions. Second try.
    
    * restore previous format
    
    * no more short names

--- src/wrappers/themis/react-native-themis/ios/RCTThemis.h
+++ src/wrappers/themis/react-native-themis/ios/RCTThemis.h
@@ -10,18 +10,22 @@
 
 #import <React/RCTBridgeModule.h>
 
-#define BYTEOVERFLOW        255
-#define CONTEXTREQUIRED     254
-#define PUBLICKEYREQUIRED   253
-#define PRIVATEKEYREQUIRED  252
+#define BYTEOVERFLOW 255
+#define CONTEXTREQUIRED 254
+#define PUBLICKEYREQUIRED 253
+#define PRIVATEKEYREQUIRED 252
+#define DESERIALIZE_ERROR 251
+#define DESERIALIZE_MEMORY 250
 
+#define BYTEOVERFLOWREASON "Byte overflow: value is out of range"
 #define CONTEXTREQUIREDREASON "Context required"
 #define PUBLICKEYREQUIREDREASON "Public key can not be null or blank"
 #define PRIVATEKEYREQUIREDREASON "Private key can not be null or blank"
+#define DESERIALIZE_ERRORREASON "Deserialize error: empty input data"
+#define DESERIALIZE_MEMORYREASON "Deserialize error: can not allocate memory"
 
-#define KEYTYPE_EC  0
+#define KEYTYPE_EC 0
 #define KEYTYPE_RSA 1
-
 
 @interface RCTThemis : NSObject <RCTBridgeModule>
 @end
