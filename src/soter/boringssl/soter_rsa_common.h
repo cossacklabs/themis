226daed33ef3e84582be02cd54de1e4b0ba49035
Oleksii Lozovskyi <me@ilammy.net>
2022-10-30 13:46:47 +0900

    soter: soter_rsa_export_key() accepts const*
    Harmonize those with the new EC exporters. Don't bother updating
    the API, RSA is legacy anyway.

--- src/soter/boringssl/soter_rsa_common.h
+++ src/soter/boringssl/soter_rsa_common.h
@@ -24,6 +24,6 @@
 int rsa_key_length(unsigned size);
 soter_status_t soter_rsa_gen_key(EVP_PKEY_CTX* pkey_ctx, unsigned key_length);
 soter_status_t soter_rsa_import_key(EVP_PKEY* pkey, const void* key, size_t key_length);
-soter_status_t soter_rsa_export_key(soter_sign_ctx_t* ctx, void* key, size_t* key_length, bool isprivate);
+soter_status_t soter_rsa_export_key(const soter_sign_ctx_t* ctx, void* key, size_t* key_length, bool isprivate);
 
 #endif /* SOTER_BORINGSSL_RSA_COMMON_H */
