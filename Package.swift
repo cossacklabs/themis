e385f6a2b4693cb0a091d855420a512709b19b51
Oleksii Radetskyi <alexey.radetsky@gmail.com>
2023-09-14 07:05:10 +0300

    Update Obj-C and Swift wrappers.  (#1020)
    * update Package.swift
    
    * update Cartfile
    
    * update Cartfile.resolved
    
    * themis.podspec: spec new dependency
    
    * set 0.15.1 version in xcodeproj
    
    * update versions in Package.swift and themis.podspec
    
    * fix projects settings
    
    * update changelog
    
    * set 0.15.1 version in examples
    
    * disable bitcode in project
    
    * update podspec
    
    * increase iOS version in Objc Tests Podfile

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
     targets: [
         .binaryTarget(name: "themis",
                       // update version in URL path
-                      url: "https://github.com/cossacklabs/themis/releases/download/0.14.0/themis.xcframework.zip",
+                      url: "https://github.com/cossacklabs/themis/releases/download/0.15.1/themis.xcframework.zip",
                       // The scripts/create_xcframework.sh calculates the checksum when generating the XCF.
                       // Alternatively, run from package directory:
                       // swift package compute-checksum build/xcf_output/themis.xcframework.zip
-                      checksum: "182587c9070b94e552816d3cc174d64ea0f0a64d5b19483210bcf3dcac4d2658"),
+                      checksum: "2d5bfbba7d5d3ecd0d40acdaee65d1b5d42dee5df5ae9d99faec4119087238c5"),
 
     ]
 )
