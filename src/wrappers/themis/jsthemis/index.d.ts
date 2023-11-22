abe62074b7d14da5f50022aece949e624e112a69
Alex Radetsky <alexey.radetsky@gmail.com>
2023-11-22 19:08:21 +0200

    Add TypeScript declaration file (index.d.ts)

---
+++ src/wrappers/themis/jsthemis/index.d.ts
@@ -0,0 +1,60 @@
+export declare class KeyPair {
+    public(): Uint8Array;
+    private(): Uint8Array;
+}
+
+export declare class SymmetricKey {
+    constructor();
+}
+
+export declare class SecureCellSeal {
+    static withKey(key: SymmetricKey): SecureCellSeal;
+    static withPassphrase(passphrase: string): SecureCellSeal;
+    encrypt(plaintext: Uint8Array, context?: Uint8Array): Uint8Array;
+    decrypt(encrypted: Uint8Array, context?: Uint8Array): Uint8Array;
+}
+
+export declare interface SecureCellTokenProtectResult {
+    data: Uint8Array;
+    token: Uint8Array;
+}
+
+export declare class SecureCellTokenProtect {
+    static withKey(key: SymmetricKey): SecureCellTokenProtect;
+    encrypt(plaintext: Uint8Array, context?: Uint8Array): SecureCellTokenProtectResult;
+    decrypt(encrypted: Uint8Array, token: Uint8Array, context?: Uint8Array): Uint8Array;
+}
+
+export declare class SecureCellContextImprint {
+    static withKey(key: SymmetricKey): SecureCellContextImprint;
+    encrypt(plaintext: Uint8Array, context: Uint8Array): Uint8Array;
+    decrypt(encrypted: Uint8Array, context: Uint8Array): Uint8Array;
+}
+
+export declare class SecureMessage {
+    constructor(privateKey: Uint8Array | null, publicKey: Uint8Array | null);
+    sign(message: Uint8Array): Uint8Array;
+    verify(message: Uint8Array): Uint8Array;
+    encrypt(message: Uint8Array): Uint8Array;
+    decrypt(message: Uint8Array): Uint8Array;
+}
+
+type GetPublicKeyCallback = (peerID: Uint8Array) => Uint8Array | null;
+
+export declare class SecureSession {
+    constructor(peerID: Uint8Array, privateKey: Uint8Array, getPublicKeyCallback: GetPublicKeyCallback);
+    isEstablished(): boolean;
+    connectRequest(): Uint8Array;
+    sendToPeer(message: Uint8Array): void;
+    receiveFromPeer(): Uint8Array;
+    wrap(message: Uint8Array): Uint8Array;
+    unwrap(message: Uint8Array): Uint8Array;
+}
+
+export declare class SecureComparator {
+    constructor(sharedSecret: Uint8Array);
+    beginCompare(): Uint8Array;
+    proceedCompare(request: Uint8Array): Uint8Array;
+    isCompareComplete(): boolean;
+    isMatch(): boolean;
+}
